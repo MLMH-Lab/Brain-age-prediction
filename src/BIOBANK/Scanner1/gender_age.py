ca35773860511930f38540fc2ef9a81573473f89
lea <lea.baecker@kcl.ac.uk>
2018-12-13 17:38:18 +0000

    Simplified histogram script, added functions to explore age distribution added filters for to exclude ages below 49 and above 75 list the participant IDs to include

--- src/BIOBANK/Scanner1/gender_age.py
+++ src/BIOBANK/Scanner1/gender_age.py
@@ -1,67 +1,75 @@
 """
 Script to explore distribution of age and gender in UK BIOBANK dataset from scanner1
-Aim is to plot a line graph of age vs number of subjects for male/female using seaborn
+Aim is to plot a line graph of age vs number of subjects for male/female
 """
 
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
-import numpy as np
 
 DATASET_DEMOGRAPHIC_FILENAME = '/home/lea/PycharmProjects/predicted_brain_age/data/BIOBANK/Scanner1/participants.tsv'
 DATASET_AVAILABLE = pd.read_csv(DATASET_DEMOGRAPHIC_FILENAME, sep='\t')
 DATASET_EXCL_NAN = DATASET_AVAILABLE.dropna()
-DATASET_MALE = DATASET_EXCL_NAN[DATASET_EXCL_NAN["Gender"]==1]
-DATASET_FEMALE = DATASET_EXCL_NAN[DATASET_EXCL_NAN["Gender"]==0]
+DATASET_MALE = DATASET_EXCL_NAN[DATASET_EXCL_NAN["Gender"] == 1]
+DATASET_FEMALE = DATASET_EXCL_NAN[DATASET_EXCL_NAN["Gender"] == 0]
 
-COL_AGE = DATASET_EXCL_NAN['Age']
-COL_GENDER = DATASET_EXCL_NAN['Gender']
 
-def age_ls(df):
-    """Returns Age columns from dataframe as list"""
-    age_list = []
+def age_dict(df, x, direction):
+    '''Creates dictionary with number of subjects per age based on df col Age'''
+    age_dict_x = {}
     for label, row in df.iterrows():
-        col_age = df.loc[label, "Age"]
-        age_list.append(int(col_age))
-    return age_list
-
-AGE_LIST_FEMALE = age_ls(DATASET_FEMALE)
-AGE_LIST_MALE = age_ls(DATASET_MALE)
-
-def age_fre(list):
-    """Returns number of instances per age as dict"""
-    age_count = {}
-    for age in list:
-        if age in age_count:
-            age_count[age] += 1
-        elif age not in age_count:
-            age_count[age] = 1
+        age = row["Age"]
+        if direction == '>':
+            if age > x:
+                if age in age_dict_x:
+                    age_dict_x[age] += 1
+                else:
+                    age_dict_x[age] = 1
+        elif direction == '<':
+            if age < x:
+                if age in age_dict_x:
+                    age_dict_x[age] += 1
+                else:
+                    age_dict_x[age] = 1
         else:
-            print("error with" + age)
-    return age_count
+            print("direction parameter has to be string < or >")
+    print(age_dict_x)
 
-AGE_FRE_FEMALE = age_fre(AGE_LIST_FEMALE)
-AGE_FRE_MALE = age_fre(AGE_LIST_MALE)
 
-AGE_FRE_FEMALE_DF = pd.DataFrame.from_dict(AGE_FRE_FEMALE, orient='index', dtype=int, columns=["Count"])
-AGE_FRE_MALE_DF = pd.DataFrame.from_dict(AGE_FRE_MALE, orient='index', dtype=int, columns=["Count"])
-AGE_FRE_FEMALE_DF["Age"] = AGE_FRE_FEMALE_DF.index
-AGE_FRE_MALE_DF["Age"] = AGE_FRE_MALE_DF.index
+# create dataset excl subjects with missing data and including ages 49-75 (see Zhao et al 2018)
+age_dict(DATASET_EXCL_NAN, 50, '<')
+age_dict(DATASET_EXCL_NAN, 70, '>')
+DATASET_AGES_49 = DATASET_EXCL_NAN[DATASET_EXCL_NAN["Age"] > 48]
+DATASET_AGES_49_75 = DATASET_AGES_49[DATASET_AGES_49["Age"] < 76]
+print(len(DATASET_EXCL_NAN))
+print(len(DATASET_AGES_49))
+print(len(DATASET_AGES_49_75))
+print(DATASET_AGES_49_75[["Participant_ID"]])
 
-age_plot_female = sns.lineplot(x="Age", y="Count", data=AGE_FRE_FEMALE_DF, legend='full', label='female', color='red')
-age_plot_male = sns.lineplot(x="Age", y="Count", data=AGE_FRE_MALE_DF, legend='full', label='male', color='blue')
+# quick way to display difference using pandas plots
+DATASET_EXCL_NAN.groupby('Gender').Age.hist(bins=range(49, 80, 1), alpha=0.5, histtype='step', lw=5)
+DATASET_AGES_50_75.groupby('Gender').Age.hist(bins=range(49, 80, 1), alpha=0.5, histtype='step', lw=5)
+
+male_ages = DATASET_EXCL_NAN.groupby('Gender').get_group(1).Age
+female_ages = DATASET_EXCL_NAN.groupby('Gender').get_group(0).Age
+plt.hist(male_ages, color='blue', histtype='step', lw=2, bins=range(47, 79, 1), label='male')
+plt.hist(female_ages, color='red', histtype='step', lw=2, bins=range(47, 79, 1), label='female')
+
 plt.title("Age distribution in UK BIOBANK")
 plt.axis('tight')
 plt.xlabel("Age [years]")
 plt.ylabel("Number of subjects")
-plt.legend(loc= 'upper right')
-plt.show()
+plt.legend(loc='upper right')
+plt.tick_params(labelsize=18)
+fig = plt.figure(figsize=(25, 15))
+plt.show(fig)
 
 plt.savefig('gender_age_dist_BIOBANK.png')
 
-#def main():
+
+# def main():
 #    dataset_demographic_filename = 2
 #    pass
 
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 #    main()
