4d24a4d7ae6734cfe8fe07648e2dffdd2574c8b5
lea <lea.baecker@kcl.ac.uk>
2019-01-14 16:11:20 +0000

    Created functions to summarise demographics (sample size, age)

--- src/BIOBANK/Scanner1/gender_age.py
+++ src/BIOBANK/Scanner1/gender_age.py
@@ -5,43 +5,90 @@ Aim is to plot a line graph of age vs number of subjects for male/female
 
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
 
-DATASET_DEMOGRAPHIC_FILENAME = '/home/lea/PycharmProjects/predicted_brain_age/data/BIOBANK/Scanner1/participants.tsv'
-DATASET_AVAILABLE = pd.read_csv(DATASET_DEMOGRAPHIC_FILENAME, sep='\t')
-DATASET_EXCL_NAN = DATASET_AVAILABLE.dropna()
-DATASET_MALE = DATASET_EXCL_NAN[DATASET_EXCL_NAN["Gender"] == 1]
-DATASET_FEMALE = DATASET_EXCL_NAN[DATASET_EXCL_NAN["Gender"] == 0]
+DATASET_DEMOGRAPHIC = pd.read_csv('/home/lea/PycharmProjects/'
+                                  'redicted_brain_age/data/BIOBANK/Scanner1/participants.tsv', sep='\t')
+DATASET_EXCL_NAN = DATASET_DEMOGRAPHIC.dropna()
 
-# create dataset excl subjects with missing data and including ages 49-75
-DATASET_AGES_49_74 = DATASET_EXCL_NAN[(DATASET_EXCL_NAN.Age > 48) & (DATASET_EXCL_NAN.Age < 74)]
-
-# quick way to display difference using pandas plots
-DATASET_EXCL_NAN.groupby('Gender').Age.hist(bins=range(45, 80, 1), alpha=0.5, histtype='step', lw=2)
-DATASET_AGES_49_74.groupby('Gender').Age.hist(bins=range(49, 74, 1), alpha=0.5, histtype='step', lw=2)
-DATASET_AGES_49_74.Age.hist()
-
-# more detailed display, select all to run
+# histogram of age disttibution by gender
 plt.style.use('seaborn-whitegrid')
 
-male_ages = DATASET_AGES_49_74.groupby('Gender').get_group(1).Age
-female_ages = DATASET_AGES_49_74.groupby('Gender').get_group(0).Age
+MALE_AGES = DATASET_EXCL_NAN.groupby('Gender').get_group(1).Age
+FEMALE_AGES = DATASET_EXCL_NAN.groupby('Gender').get_group(0).Age
 
-plt.hist(male_ages, color='blue', histtype='step', lw=2, bins=range(47, 79, 1), label='male')
-plt.hist(female_ages, color='red', histtype='step', lw=2, bins=range(47, 79, 1), label='female')
 
-plt.title("Age distribution in UK BIOBANK")
-plt.axis('tight')
-plt.xlabel("Age [years]")
-plt.ylabel("Number of subjects")
-plt.xticks(range(45, 80, 5))
-plt.yticks(range(0, 400, 50))
-plt.legend(loc='upper right')
-plt.tick_params(labelsize=10)
-fig = plt.figure(figsize=(25, 15))
-plt.show(fig)
+def plot_save_histogram():
+    """Creates histogram of age distribution by gender and saves in output folder as png"""
+    plt.figure(figsize=(10, 7))
+    plt.hist(MALE_AGES, color='blue', histtype='step', lw=2, bins=range(45, 75, 1), label='male')
+    plt.hist(FEMALE_AGES, color='red', histtype='step', lw=2, bins=range(45, 75, 1), label='female')
+    plt.title("Age distribution in UK BIOBANK", fontsize=17)
+    plt.axis('tight')
+    plt.xlabel("Age at MRI scan [years]", fontsize=15)
+    plt.ylabel("Number of subjects", fontsize=15)
+    plt.xticks(range(45, 75, 5))
+    plt.yticks(range(0, 401, 50))
+    plt.legend(loc='upper right', fontsize=13)
+    plt.tick_params(labelsize=13)
+    plt.savefig('/home/lea/PycharmProjects/predicted_brain_age/outputs/gender_age_dist_BIOBANK.png')
+    plt.show()
 
-plt.savefig('gender_age_dist_BIOBANK.png')
+
+plot_save_histogram()
+
 
 """Script to look at demographics info based on Zhao et al 2018
 Required are: number of subjects, gender split, age range,"""
+
+N_TOTAL = len(DATASET_EXCL_NAN)
+N_MALE = len(DATASET_EXCL_NAN.groupby('Gender').get_group(1))
+N_FEMALE = len(DATASET_EXCL_NAN.groupby('Gender').get_group(0))
+
+AGE_MEAN = DATASET_EXCL_NAN.Age.mean()
+AGE_MEDIAN = DATASET_EXCL_NAN.Age.median()
+AGE_SD = DATASET_EXCL_NAN.Age.std()
+AGE_RANGE_MIN = DATASET_EXCL_NAN.Age.min()
+AGE_RANGE_MAX = DATASET_EXCL_NAN.Age.max()
+
+AGE_MALE_MEAN = DATASET_EXCL_NAN.groupby('Gender').get_group(1).Age.mean()
+AGE_FEMALE_MEAN = DATASET_EXCL_NAN.groupby('Gender').get_group(0).Age.mean()
+AGE_MALE_MEDIAN = DATASET_EXCL_NAN.groupby('Gender').get_group(1).Age.median()
+AGE_FEMALE_MEDIAN = DATASET_EXCL_NAN.groupby('Gender').get_group(0).Age.median()
+AGE_MALE_SD = DATASET_EXCL_NAN.groupby('Gender').get_group(1).Age.std()
+AGE_FEMALE_SD = DATASET_EXCL_NAN.groupby('Gender').get_group(0).Age.std()
+AGE_MALE_RANGE_MIN = DATASET_EXCL_NAN.groupby('Gender').get_group(1).Age.min()
+AGE_FEMALE_RANGE_MIN = DATASET_EXCL_NAN.groupby('Gender').get_group(0).Age.min()
+AGE_MALE_RANGE_MAX = DATASET_EXCL_NAN.groupby('Gender').get_group(1).Age.max()
+AGE_FEMALE_RANGE_MAX = DATASET_EXCL_NAN.groupby('Gender').get_group(0).Age.max()
+
+
+def display_demographics():
+    """Prints demographic information about sample size and age"""
+    print("Sample sizes [Total (m / f)]: %d (%d / %d)" % (N_TOTAL, N_MALE, N_FEMALE))
+    print("Age - total [mean (SD), min-max]: %5.2f (%5.2f), %5.2f-%5.2f"
+          % (AGE_MEAN, AGE_SD, AGE_RANGE_MIN, AGE_RANGE_MAX))
+    print("Age - male [mean (SD), min-max]: %5.2f (%5.2f), %5.2f-%5.2f"
+          % (AGE_MALE_MEAN, AGE_MALE_SD, AGE_MALE_RANGE_MIN, AGE_MALE_RANGE_MAX))
+    print("Age - female [mean (SD), min-max]: %5.2f (%5.2f), %5.2f-%5.2f"
+          % (AGE_FEMALE_MEAN, AGE_FEMALE_SD, AGE_FEMALE_RANGE_MIN, AGE_FEMALE_RANGE_MAX))
+
+
+display_demographics()
+
+
+def create_dem_file():
+    """Saves display_demographics info to a txt file in output folder"""
+    file = open('/home/lea/PycharmProjects/predicted_brain_age/outputs/demographics.txt', 'w')
+    file.write("Demographics for UK BIOBANK Scanner1 \n \n")
+    file.write("Sample sizes [Total (m / f)]: %d (%d / %d) \n" % (N_TOTAL, N_MALE, N_FEMALE))
+    file.write("Age - total [mean (SD), min-max]: %5.2f (%5.2f), %5.2f-%5.2f \n"
+               % (AGE_MEAN, AGE_SD, AGE_RANGE_MIN, AGE_RANGE_MAX))
+    file.write("Age - male [mean (SD), min-max]: %5.2f (%5.2f), %5.2f-%5.2f \n"
+               % (AGE_MALE_MEAN, AGE_MALE_SD, AGE_MALE_RANGE_MIN, AGE_MALE_RANGE_MAX))
+    file.write("Age - female [mean (SD), min-max]: %5.2f (%5.2f), %5.2f-%5.2f \n"
+               % (AGE_FEMALE_MEAN, AGE_FEMALE_SD, AGE_FEMALE_RANGE_MIN, AGE_FEMALE_RANGE_MAX))
+    file.close()
+    return file
+
+
+create_dem_file()
