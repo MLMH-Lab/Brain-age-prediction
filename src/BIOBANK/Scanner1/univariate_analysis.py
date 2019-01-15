845760ea6f2546ffbd0ed040328e60138903e9ff
Lea <k1802521@kcl.ac.uk>
2019-01-15 17:36:56 +0000

    Expanded normalise function to extract participant ID + output array

--- src/BIOBANK/Scanner1/univariate_analysis.py
+++ src/BIOBANK/Scanner1/univariate_analysis.py
@@ -10,28 +10,47 @@ References
 Cerebral Cortex (2018).
 """
 
-from pathlib import Path
-
 import numpy as np
 import pandas as pd
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
+# import statsmodels.api as sm
+# import statsmodels.formula.api as smf
 
-PROJECT_ROOT = Path('../../../')  # does not work for loading the below file
+# from pathlib import Path
+
+# load freesurfer dataset and change column names to a usable format
+# PROJECT_ROOT = Path('../../../')
+# dataset_fs_all_regions = pd.read_csv(PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner1' / 'freesurferData.csv')
 dataset_fs_all_regions = pd.read_csv('/home/lea/PycharmProjects/'
                                      'predicted_brain_age/data/BIOBANK/Scanner1/freesurferData.csv')
-
-# some column names contain '-' which is not allowed, so they are replaced with '_'
 dataset_fs_all_regions_renamed = dataset_fs_all_regions.rename(columns=lambda x: x.replace('-', '_'))
-dataset_fs_all_regions_renamed.columns
+
+# load demographic dataset to access age of participants
+# dataset_demographic = pd.read_csv(PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner1' / 'participants.tsv', sep='\t')
+dataset_demographic = pd.read_csv('/home/lea/PycharmProjects/'
+                                  'predicted_brain_age/data/BIOBANK/Scanner1/participants.tsv', sep='\t')
+dataset_demographic_excl_nan = dataset_demographic.dropna()
+
+# check if both datasets contain same number of participants
+print(len(dataset_demographic_excl_nan))
+print(len(dataset_fs_all_regions_renamed))
 
 
 def normalise_region(total_volume, region_volume):
-    """Normalise each brain region by dividing the regional by the total intracranvial volume"""
+    """Normalise regional volume, extract participant IDs,
+    and return array of participant IDs and normalised regional volumes"""
+
     total = np.array(dataset_fs_all_regions_renamed[total_volume])
     region = np.array(dataset_fs_all_regions_renamed[region_volume])
     region_normalised = region / total
-    print(region_normalised)
+
+    # Create new column Participant_ID from Image_ID
+    dataset_fs_all_regions_renamed["Participant_ID"] = dataset_fs_all_regions_renamed['Image_ID']. \
+        str.split('_', expand=True)[0]
+    participant_id = np.array(dataset_fs_all_regions_renamed["Participant_ID"])
+
+    # Create new array
+    normalised_array = np.array([participant_id, region_normalised])
+    return normalised_array
 
 
 # test function
