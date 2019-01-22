c114d221e2ec738afc04fc2fbd24ad4ac9a023d0
Lea <k1802521@kcl.ac.uk>
2019-01-22 16:14:06 +0000

    added function to create frequency bar plots

--- src/BIOBANK/Scanner1/dem_bias.py
+++ src/BIOBANK/Scanner1/dem_bias.py
@@ -2,16 +2,28 @@
 Supplementary data and labels acquired from https://biobank.ctsu.ox.ac.uk/crystal/search.cgi"""
 
 import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def fre_plot(df, col_name):
+    """Frequency plot of a dataframe column to visually assess distribution"""
+
+    df[col_name].value_counts().plot('bar')
+    plt.show()
+
+# Test fre_plot function
+fre_plot(dataset_dem_excl_nan, 'Gender')
+fre_plot(dataset_dem_excl_nan, 'Ethnicity')
 
 
 def main():
 
     # Loading supplementary demographic data
-    dataset_demographic_suppl = pd.read_csv(
+    dataset_dem = pd.read_csv(
         '/home/lea/PycharmProjects/predicted_brain_age/data/BIOBANK/Scanner1/ukb22321.csv',
         usecols=['eid', '31-0.0', '21003-2.0', '21000-0.0'])
-    dataset_demographic_suppl.columns = ['ID', 'Gender', 'Ethnicity', 'Age at MRI']
-    dataset_demographic_suppl_excl_nan = dataset_demographic_suppl.dropna()
+    dataset_dem.columns = ['ID', 'Gender', 'Ethnicity', 'Age']
+    dataset_dem_excl_nan = dataset_dem.dropna()
 
 
 if __name__ == "__main__":
