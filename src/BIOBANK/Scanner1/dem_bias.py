2d7f995df3063907ac2d824aa6dc9c3988fe57b2
Lea <k1802521@kcl.ac.uk>
2019-01-23 15:05:11 +0000

    added variables on expected vs observed age frequency per age group + performed chisquare test

--- src/BIOBANK/Scanner1/dem_bias.py
+++ src/BIOBANK/Scanner1/dem_bias.py
@@ -70,14 +70,35 @@ def main():
     # Export ethnicity distribution
     fre_table(dataset_dem_excl_nan_grouped, 'Ethnicity')
 
-    # create lists of male and female ages
+    # Create lists of male and female ages
     male_code = 1
     female_code = 0
 
     male_ages = list(dataset_dem_excl_nan_grouped.groupby('Gender').get_group(male_code).Age)
     female_ages = list(dataset_dem_excl_nan_grouped.groupby('Gender').get_group(female_code).Age)
 
+    # Perform Pearson chi-squared test
+    n_total = len(dataset_dem_excl_nan_grouped['Age'])
+    n_gender_expected = n_total / 2
+    n_male_observed = len(male_ages)
+    n_female_observed = len(female_ages)
 
+    age_numbers = len(dataset_dem_excl_nan_grouped['Age'].value_counts())
+    subjects_expected_per_age = n_total / age_numbers
+    gender_expected_per_age = subjects_expected_per_age / 2
+    gender_expected = [gender_expected_per_age] * age_numbers
+
+    gender_observed_per_age = pd.crosstab(dataset_dem_excl_nan_grouped['Gender'], dataset_dem_excl_nan_grouped['Age']).transpose()
+    female_observed_per_age = gender_observed_per_age['Female']
+    male_observed_per_age = gender_observed_per_age['Male']
+
+    chi2, p = stats.chisquare(male_observed_per_age, gender_expected)
+    msg = "Test Statistic: {}\np-value: {}"
+    print(msg.format(chi2, p))
+
+    chi2, p = stats.chisquare(female_observed_per_age, gender_expected)
+    msg = "Test Statistic: {}\np-value: {}"
+    print(msg.format(chi2, p))
 
 
 if __name__ == "__main__":
