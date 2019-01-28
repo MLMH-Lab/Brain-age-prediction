2b63c5b8f4334b3d0327dce5eccd09aa492bfd69
Lea <k1802521@kcl.ac.uk>
2019-01-28 14:00:02 +0000

    Added description and steps based on trello to-do list

---
+++ src/BIOBANK/Scanner1/svm.py
@@ -0,0 +1,16 @@
+"""Script to implement SVM in BIOBANK Scanner1 freesurfer data to predict brain age
+
+Step 1: generate dataset in hdf5 format
+Step 2: Set global random seed
+Step 3: Normalise by TiV
+Step 4: Prepare CV vaoran;es
+Step 5: Create loops for repetitions and folds
+Step 6: Split into training and test sets
+Step 7: Scaling
+Step 8: Declare search space
+Step 9: Perform search with nested CV
+Step 10: Retrain best model with whole training set
+Step 11: Predict test set
+Step 12: Print R_squared, MAE, RMSE
+Step 13: Save model file, scaler file, predictions file
+Step 16: Print CV results"""
