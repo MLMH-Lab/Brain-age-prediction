3c57d02d083be890bf2e076d9db8f284d25a63aa
Lea <k1802521@kcl.ac.uk>
2019-01-29 11:58:42 +0000

    Corrected notes, set random seed using np as well as random

--- src/BIOBANK/Scanner1/svm.py
+++ src/BIOBANK/Scanner1/svm.py
@@ -1,29 +1,30 @@
 """Script to implement SVM in BIOBANK Scanner1 freesurfer data to predict brain age
 
-Step 1: generate dataset in hdf5 format
-Step 2: Set global random seed
-Step 3: Normalise by TiV
-Step 4: Prepare CV vaoran;es
-Step 5: Create loops for repetitions and folds
-Step 6: Split into training and test sets
-Step 7: Scaling
-Step 8: Declare search space
-Step 9: Perform search with nested CV
-Step 10: Retrain best model with whole training set
-Step 11: Predict test set
-Step 12: Print R_squared, MAE, RMSE
-Step 13: Save model file, scaler file, predictions file
-Step 16: Print CV results"""
+Step 1: Set global random seed
+Step 2: Normalise by TiV
+Step 3: Prepare CV variables
+Step 4: Create loops for repetitions and folds
+Step 5: Split into training and test sets
+Step 6: Scaling
+Step 7: Declare search space
+Step 8: Perform search with nested CV
+Step 9: Retrain best model with whole training set
+Step 10: Predict test set
+Step 11: Print R_squared, MAE, RMSE
+Step 12: Save model file, scaler file, predictions file
+Step 13: Print CV results"""
 
 from pathlib import Path
 import pandas as pd
 import numpy as np
+import random
 
 
 def main():
 
     # Initialise random number generator with random seed
     np.random.seed(30)
+    random.seed(30)
 
 if __name__ == "__main__":
     main()
