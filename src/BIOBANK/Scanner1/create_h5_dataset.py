3b6ac268c841a53343ec664c8702800446ed1aba
Lea <k1802521@kcl.ac.uk>
2019-01-30 13:59:56 +0000

    updated demographic file imported to be the homogeneous dataset

--- src/BIOBANK/Scanner1/create_h5_dataset.py
+++ src/BIOBANK/Scanner1/create_h5_dataset.py
@@ -13,15 +13,14 @@ def main():
     dataset_freesurfer = pd.read_csv(PROJECT_ROOT / 'data/BIOBANK/Scanner1/freesurferData.csv')
 
     # Loading demographic data to access age per participant
-    dataset_demographic = pd.read_csv(PROJECT_ROOT / 'data/BIOBANK/Scanner1/participants.tsv', sep='\t')
-    dataset_demographic_excl_nan = dataset_demographic.dropna()
+    dataset_demographic = pd.read_csv(PROJECT_ROOT / 'data/BIOBANK/Scanner1/homogeneous_dataset.csv')
 
-    # Create a new col in FS dataset to contain Participant_ID
-    dataset_freesurfer['Participant_ID'] = dataset_freesurfer['Image_ID']. \
+    # Create a new col in FS dataset to contain participant ID
+    dataset_freesurfer['ID'] = dataset_freesurfer['Image_ID']. \
         str.split('_', expand=True)[0]
 
     # Merge FS dataset and demographic dataset to access age
-    dataset_csv = pd.merge(dataset_demographic_excl_nan[['Participant_ID', 'Age']], dataset_freesurfer, on='Participant_ID')
+    dataset_csv = pd.merge(dataset_demographic[['ID', 'Age']], dataset_freesurfer, on='ID')
 
     # Create dataset as hdf5
     dataset_hdf = dataset_csv.to_hdf(PROJECT_ROOT / 'data/BIOBANK/Scanner1/freesurferData.h5', key='table', mode='w')
