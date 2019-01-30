fac35df1b3e977b037a155f53a995b1a3e0f2f1e
Lea <k1802521@kcl.ac.uk>
2019-01-30 11:52:09 +0000

    removed print of skf

--- src/BIOBANK/Scanner1/svm.py
+++ src/BIOBANK/Scanner1/svm.py
@@ -49,7 +49,6 @@ def main():
     # Create 10-fold cross-validator, stratified by age
     skf = StratifiedKFold(n_splits=10)
     skf.get_n_splits(regions_norm, age)
-    print(skf)
     for train_index, test_index in skf.split(regions_norm, age):
         print("TRAIN:", train_index, "TEST:", test_index)
     X_train, X_test = regions_norm[train_index], regions_norm[test_index]
