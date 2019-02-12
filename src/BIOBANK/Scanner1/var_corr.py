2c2d368197b63fbc4a6a2197fee6e3b1c6fd37c7
Lea <k1802521@kcl.ac.uk>
2019-02-12 17:04:29 +0000

    Added additional variable information

--- src/BIOBANK/Scanner1/var_corr.py
+++ src/BIOBANK/Scanner1/var_corr.py
@@ -1,14 +1,28 @@
 """Script to assess correlations between difference in actual and
 predicted age with demographic variables in UK BIOBANK Scanner1
 
-Variables to assess [variable code - variable name]:
-6138 - Which of the following qualifications do you have? (multi code)
-24005 - Particulate matter air pollution (pm10); 2010
-24009 - Traffic intensity on the nearest road
-24010 - Inverse distance to the nearest road
-24014 - Close to major road
-24500 - Greenspace percentage
-24501 - Domestic garden percentage
-24502 - Water percentage
-24506 - Natural environment percentage
+Variables to assess [variable code - variable name, code names]:
+6138-2.0, 6138-2.1, 6138-2.2, 6138-2.3, 6138-2.4, 6138-2.4 - Which of the following qualifications do you have? (up to 5 selections possible)
+1	College or University degree
+2	A levels/AS levels or equivalent
+3	O levels/GCSEs or equivalent
+4	CSEs or equivalent
+5	NVQ or HND or HNC or equivalent
+6	Other professional qualifications eg: nursing, teaching
+-7	None of the above
+-3	Prefer not to answer
+
+24005-0.0 - Particulate matter air pollution (pm10); 2010
+24009-0.0 - Traffic intensity on the nearest road
+24010-0.0 - Inverse distance to the nearest road
+24014-0.0 - Close to major road (binary)
+24500-0.0 - Greenspace percentage
+24501-0.0 - Domestic garden percentage
+24502-0.0 - Water percentage
+24506-0.0 - Natural environment percentage
+
+Note: Baseline assessment data chosen for 24500-24506 because data is not available for all subjects at next assessment;
+no data available for imaging assessment
+
+Variable information available at https://biobank.ctsu.ox.ac.uk/crystal/label.cgi;
 """
