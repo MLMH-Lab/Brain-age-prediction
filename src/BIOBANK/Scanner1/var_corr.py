872c0df94c7e567d2d97bb9d33bf5f2bbdd745e7
Lea <k1802521@kcl.ac.uk>
2019-02-12 15:20:15 +0000

    Created var_corr py file and added description

---
+++ src/BIOBANK/Scanner1/var_corr.py
@@ -0,0 +1,14 @@
+"""Script to assess correlations between difference in actual and
+predicted age with demographic variables in UK BIOBANK Scanner1
+
+Variables to assess [variable code - variable name]:
+6138 - Which of the following qualifications do you have? (multi code)
+24005 - Particulate matter air pollution (pm10); 2010
+24009 - Traffic intensity on the nearest road
+24010 - Inverse distance to the nearest road
+24014 - Close to major road
+24500 - Greenspace percentage
+24501 - Domestic garden percentage
+24502 - Water percentage
+24506 - Natural environment percentage
+"""
