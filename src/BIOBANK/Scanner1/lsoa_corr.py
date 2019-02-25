d834166a1960afdd5bddc5eda972ec95278f4916
Lea <k1802521@kcl.ac.uk>
2019-02-25 15:51:47 +0000

    Created new script for lsoa correlation analyses + added description and empty main function

---
+++ src/BIOBANK/Scanner1/lsoa_corr.py
@@ -0,0 +1,10 @@
+"""Script to assess correlations between age prediction error
+and LSOA variables (English index of multiple deprivation"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
