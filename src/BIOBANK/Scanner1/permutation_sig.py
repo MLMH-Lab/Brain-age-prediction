5e4bf76ca4e8423059938d10e95afde50efb4f07
Lea <k1802521@kcl.ac.uk>
2019-03-06 14:11:36 +0000

    Loaded coef and scores files

--- src/BIOBANK/Scanner1/permutation_sig.py
+++ src/BIOBANK/Scanner1/permutation_sig.py
@@ -1,7 +1,20 @@
 """Significance testing of SVM permutations for BIOBANK Scanner1"""
 
+from pathlib import Path
+import numpy as np
+
+PROJECT_ROOT = Path('/home/lea/PycharmProjects/predicted_brain_age')
+
 def main():
-    pass
+    # Define what subjects were modeled: total, male or female
+    subjects = 'total'
+
+    # Load permutation coefficients
+    perm_coef = np.load(PROJECT_ROOT / 'outputs' / 'permutations' / subjects / 'perm_coef.npy')
+
+    # Load permutation scores
+    perm_scores = np.load(PROJECT_ROOT / 'outputs' / 'permutations' / subjects / 'perm_scores.npy')
+
 
 if __name__ == "__main__":
     main()
