0e38b7023080403d733fd2d4674a79f6833fcf20
Lea <k1802521@kcl.ac.uk>
2019-04-16 18:07:40 +0100

    replaced csv in script with h5, added print of file_path for testing, changed output dir to be specific per bootstrap

--- src/BIOBANK/Scanner1/svm_bootstrap.py
+++ src/BIOBANK/Scanner1/svm_bootstrap.py
@@ -9,12 +9,13 @@ from src.BIOBANK.Scanner1.svm import run_svm
 def main():
     # Loop over the 50 hdf5 datasets created in script create_h5_bootstrap
     for file_path in glob.iglob(
-            '/home/lea/PycharmProjects/predicted_brain_age/data/BIOBANK/Scanner1/bootstrap/h5_datasets/homogeneous_bootstrap_*.csv',
+            '/home/lea/PycharmProjects/predicted_brain_age/data/BIOBANK/Scanner1/bootstrap/h5_datasets/homogeneous_bootstrap_*.h5',
             recursive=True):
+        print(file_path)
         file_name = os.path.basename(file_path)
         print(file_name)
-        run_svm(input_dataset=file_name,
-                output_dir='/home/lea/PycharmProjects/predicted_brain_age/outputs/bootstrap/svm/file_name')
+        run_svm(input_dataset=file_path,
+                output_dir=str('/home/lea/PycharmProjects/predicted_brain_age/outputs/bootstrap/svm/' + file_name))
 
 
 if __name__ == "__main__":
