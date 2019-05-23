5afbcb065db0f35256a63dc6e58bbdda9cf8ca54
Walter <walhugolp@gmail.com>
2019-05-23 13:06:29 +0100

    Prioritize use of participants.tsv

--- create_h5_dataset.py
+++ create_h5_dataset.py
@@ -17,20 +17,14 @@ def main():
     dataset_balanced = pd.merge(dataset_fs, ids_homogeneous, on='Image_ID')
 
     # Loading demographic data to access age per participant
-    dataset_dem = pd.read_csv(str(PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner1' / 'ukb22321.csv'),
-                              usecols=['eid', '31-0.0', '21003-2.0'])
-    dataset_dem.columns = ['ID', 'Gender', 'Age']
+    dataset_dem = pd.read_csv(PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner1' / 'participants.tsv', sep='\t')
     dataset_dem = dataset_dem.dropna()
 
     # Create a new col in FS dataset to contain participant ID
-    dataset_balanced['Participant_ID'] = dataset_balanced['Image_ID']. \
-        str.split('_', expand=True)[0]
-    dataset_balanced['ID'] = dataset_balanced['Participant_ID']. \
-        str.split('-', expand=True)[1]
-    dataset_balanced['ID'] = pd.to_numeric(dataset_balanced['ID'])
+    dataset_balanced['Participant_ID'] = dataset_balanced['Image_ID'].str.split('_', expand=True)[0]
 
     # Merge FS dataset and demographic dataset to access age
-    dataset_csv = pd.merge(dataset_dem, dataset_balanced, on='ID')
+    dataset_csv = pd.merge(dataset_dem, dataset_balanced, on='Participant_ID')
 
     # Create datasets as hdf5
     dataset_csv.to_hdf(PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner1' / 'freesurferData_total.h5', key='table',
