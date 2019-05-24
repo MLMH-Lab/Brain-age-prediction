6255df9e0deaf748e343169032ebbb34230842c9
Walter <walhugolp@gmail.com>
2019-05-24 11:10:34 +0100

    Draft

--- permutation_sig.py
+++ permutation_sig.py
@@ -7,76 +7,58 @@ import os
 
 from sklearn.externals import joblib
 
-PROJECT_ROOT = Path('/home/lea/PycharmProjects/predicted_brain_age')
+from utils import COLUMNS_NAME
+
+PROJECT_ROOT = Path.cwd()
+
+
+
 
 def main():
-    # Define what subjects were modeled: total, male or female
-    subjects = 'total'
+    # Create output subdirectory
+    experiment_name = 'total'
 
     # Define number of repetitions and folds used in SVM models
     n_repetitions = 10
     n_folds = 10
-    n_models = n_repetitions * n_folds
 
-    # Define number of subjects, number of permutations run, number of features, number of output scores
-    n_subjects = 12190
-    n_perm = 882 # 118 of the 1000 planned failed
-    n_features = 101 # freesurfer regions
+    n_perm = 1000
+    n_features = len(COLUMNS_NAME)
     n_scores = 3 # r2, MAE, RMSE
 
     # Directory where SVM models are saved
-    model_output_dir = Path(str(PROJECT_ROOT / 'outputs' / subjects))
+    model_output_dir = PROJECT_ROOT / 'outputs' / experiment_name
 
     # Directory where permutation scores and coefficients are saved (2 files per permutation)
-    perm_output_dir = Path(str(PROJECT_ROOT / 'outputs' / 'permutations' / subjects))
+    perm_dir = PROJECT_ROOT / 'outputs' / experiment_name / 'permutations'
 
     # Significance level alpha with Bonferroni correction
     bonferroni_alpha = 0.05 / n_perm
 
-
-    # ASSESSING SIGNIFICANCE OF MODEL SCORES (R2, MAE, RMSE)
-
-    # Create a list of all model score files
-    list_model_score_files = []
-    for rep in range(n_repetitions):
-        for fold in range(n_folds):
-            list_model_score_files.append(str(rep) + '_' + str(fold) + '_svm_scores.npy')
-
-    # Load SVM model scores into array model_scores
-    model_scores = np.zeros([n_models, n_scores])
-
-    index = 0
-    for score_file in list_model_score_files:
-        if os.path.isfile(str(model_output_dir / score_file)):
-            model_score = np.load(model_output_dir / score_file)
-            model_scores[index] = model_score
-        else:
-            print("File not found: %s" % score_file)
-        index += 1
-
-    model_scores_abs = np.abs(model_scores)
-    model_scores_mean = np.mean(model_scores_abs, axis=0)
-
-    # Load permutation scores into array perm_scores
+    # Assessing significance
+    perm_coefs = []
     perm_scores = []
-    counter = 0
-    for i in range(n_perm):
-        score_file_name = 'perm_scores_%04d.npy' % i
-        if os.path.isfile(str(perm_output_dir / score_file_name)):
-            perm_scores.append(np.load(perm_output_dir / score_file_name))
-        else:
-            counter = counter +1
-            print('File not found: perm_scores_%04d.npy' % i)
+    for i_perm in range(n_perm):
+
+        filepath_coef = perm_dir / ('perm_coef_{:04d}.npy'.format(i_perm))
+        filepath_scores = perm_dir / ('perm_scores_{:04d}.npy'.format(i_perm))
+
+        try:
+            perm_coefs.append(np.load(filepath_coef))
+        except FileNotFoundError:
+            print('File not found: {:}'.format(filepath_coef))
+
+        try:
+            perm_scores.append(np.load(filepath_scores))
+        except FileNotFoundError:
+            print('File not found: {:}'.format(filepath_scores))
 
     perm_scores = np.asarray(perm_scores, dtype='float32')
+    perm_coefs = np.asarray(perm_coefs, dtype='float32')
 
+    # TODO: Read assessed model values
     # Calculate proportion of permutation scores higher than model scores out of all permutations (p value)
     scores_pval = []
-    print('R_square')
-    pval = (np.sum(model_scores_mean[0] <= perm_scores[0,i]) + 1.0) / (perm_scores.shape[0] + 1)
-    scores_pval.append(pval)
-    print(pval)
-
     for i in range(1, model_scores_mean.shape[0]):
         print(i)
         pval = (np.sum(model_scores_mean[i] >= perm_scores[:,i]) + 1.0) / (perm_scores.shape[0] + 1)
@@ -88,7 +70,8 @@ def main():
     scores_sig = scores_pval_array < bonferroni_alpha
 
     # Save as csv
-    scores_csv = pd.DataFrame([model_scores_mean, scores_pval, scores_sig], columns=['R2', 'MAE', 'RMSE'],
+    scores_csv = pd.DataFrame([model_scores_mean, scores_pval, scores_sig],
+                              columns=['R2', 'MAE', 'RMSE'],
                               index=['score', 'p', 'significance'])
     scores_csv.to_csv(str(model_output_dir / 'scores_sig.csv'))
 
@@ -138,13 +121,9 @@ def main():
     coef_p_array = np.array(coef_pval)
     coef_sig_array = coef_p_array < bonferroni_alpha
 
-    # Load FS data to access feature names
-    fs_file_name = 'freesurferData_' + subjects + '.h5'
-    dataset = pd.read_hdf(PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner1' / fs_file_name, key='table')
-    feature_names = np.array(dataset.columns[5:-1])
-
     # Save as csv
-    coef_csv = pd.DataFrame([model_coef_mean, coef_p_array, coef_sig_array], columns=feature_names,
+    coef_csv = pd.DataFrame([model_coef_mean, coef_p_array, coef_sig_array],
+                            columns=COLUMNS_NAME,
                             index=['coefficient', 'pval', 'significance'])
     coef_csv = coef_csv.transpose()
     coef_csv = coef_csv.sort_values(by=['pval'])
