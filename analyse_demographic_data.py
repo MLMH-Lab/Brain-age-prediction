9e51690c8a248be3d098d8cf7ab60f86b1a5af49
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2019-05-25 11:54:43 +0100

    Merge gende_age to dem_bias

--- analyse_demographic_data.py
+++ analyse_demographic_data.py
@@ -15,6 +15,7 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 import scipy.stats as stats
+import matplotlib.pyplot as plt
 
 PROJECT_ROOT = Path.cwd()
 
@@ -45,7 +46,6 @@ def check_balance_across_groups(crosstab_df):
     return True, [None]
 
 
-
 def get_problematic_group(crosstab_df):
     """Perform contingency analysis of the subjects gender."""
     balance_flag, problematic_groups = check_balance_across_groups(crosstab_df)
@@ -54,8 +54,8 @@ def get_problematic_group(crosstab_df):
         return None
 
     conditions_proportions = crosstab_df.apply(lambda r: r / r.sum(), axis=0)
-    median_proportion = np.median(conditions_proportions.values[0,:])
-    problematic = conditions_proportions[problematic_groups].values[0,:]
+    median_proportion = np.median(conditions_proportions.values[0, :])
+    problematic = conditions_proportions[problematic_groups].values[0, :]
 
     problematic_group = problematic_groups[np.argmax(np.abs(problematic - median_proportion))]
 
@@ -74,7 +74,7 @@ def get_balanced_dataset(demographic_df):
 
         condition_unbalanced = crosstab_df[problematic_group].idxmax()
 
-        problematic_group_mask = (demographic_df['Age'] == problematic_group) &\
+        problematic_group_mask = (demographic_df['Age'] == problematic_group) & \
                                  (demographic_df['Gender'] == condition_unbalanced)
 
         list_to_drop = list(demographic_df[problematic_group_mask].sample(1).index)
@@ -82,6 +82,26 @@ def get_balanced_dataset(demographic_df):
         demographic_df = demographic_df.drop(list_to_drop, axis=0)
 
     return demographic_df
+
+
+def plot_save_histogram(male_ages, female_ages):
+    """Create histogram of age distribution by gender and saves in output folder as png"""
+    plt.style.use('seaborn-whitegrid')
+    plt.figure(figsize=(10, 7))
+
+    plt.hist(male_ages, color='blue', histtype='step', lw=2, bins=range(45, 75, 1), label='male')
+    plt.hist(female_ages, color='red', histtype='step', lw=2, bins=range(45, 75, 1), label='female')
+
+    plt.title("Age distribution in UK BIOBANK by gender", fontsize=17)
+    plt.axis('tight')
+    plt.xlabel("Age at MRI scan [years]", fontsize=15)
+    plt.ylabel("Number of subjects", fontsize=15)
+    plt.xticks(range(45, 75, 5))
+    plt.yticks(range(0, 401, 50))
+    plt.legend(loc='upper right', fontsize=13)
+    plt.tick_params(labelsize=13)
+
+    plt.savefig(str(PROJECT_ROOT / 'outputs' / 'gender_age_dist_BIOBANK.png'))
 
 
 def main():
@@ -95,7 +115,7 @@ def main():
     dataset_dem = pd.read_csv(PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner1' / 'ukb22321.csv',
                               usecols=['eid', '31-0.0', '21003-2.0', '21000-0.0'])
     dataset_dem.columns = ['ID', 'Gender', 'Ethnicity', 'Age']
-    dataset_dem_excl_nan = dataset_dem.dropna()
+    dataset_dem = dataset_dem.dropna()
 
     # Create a new 'ID' column in FS dataset to match supplementary demographic data
     dataset_fs['ID'] = dataset_fs['Image_ID'].str.split('_', expand=True)[0]
@@ -103,7 +123,7 @@ def main():
     dataset_fs['ID'] = pd.to_numeric(dataset_fs['ID'])
 
     # Merge supplementary demographic data with freesurfer data
-    dataset = pd.merge(dataset_fs, dataset_dem_excl_nan, on='ID')
+    dataset = pd.merge(dataset_fs, dataset_dem, on='ID')
 
     # Labeling data
     ethnicity_dict = {
@@ -141,6 +161,19 @@ def main():
 
     homogeneous_ids = pd.DataFrame(dataset['Image_ID'])
     homogeneous_ids.to_csv(PROJECT_ROOT / 'outputs' / 'homogeneous_dataset.csv', index=False)
+
+    male_ages = dataset.groupby('Gender').get_group('Male').Age
+    female_ages = dataset.groupby('Gender').get_group('Female').Age
+
+    plot_save_histogram(male_ages, female_ages)
+
+    print('Whole dataset')
+    print(dataset.Age.describe())
+    dataset.Age.describe().to_csv(PROJECT_ROOT / 'outputs' / 'scanner01_whole_dataset_dem.csv')
+
+    print('Grouped dataset')
+    print(dataset.groupby('Gender').Age.describe())
+    dataset.groupby('Gender').Age.describe().to_csv(PROJECT_ROOT / 'outputs' / 'scanner01_grouped_dem.csv')
 
 
 if __name__ == "__main__":
