7fd2b34cf6b71aec347b22d6d2c6efb4ff208aaf
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2019-06-09 15:37:07 +0100

    extend load_demographic_data to read participants.tsv files

--- helper_functions.py
+++ helper_functions.py
@@ -6,9 +6,38 @@ import pandas as pd
 
 def load_demographic_data(demographic_path, id_path):
     """Load dataset using selected ids."""
-    # Loading demographic data
-    demographic_df = pd.read_csv(demographic_path, usecols=['eid', '31-0.0', '21000-0.0', '21003-2.0'])
-    demographic_df.columns = ['ID', 'Gender', 'Ethnicity', 'Age']
+
+    demographic_df = pd.read_csv(demographic_path)
+
+    # if using UK Biobank supplementary data
+    if 'eid' in demographic_df.columns:
+        demographic_df = demographic_df[['eid', '31-0.0', '21000-0.0', '21003-2.0']]
+        demographic_df.columns = ['ID', 'Gender', 'Ethnicity', 'Age']
+
+        # Labeling data
+        ethnicity_dict = {
+            1: 'White', 1001: 'White', 1002: 'White', 1003: 'White',
+            2: 'Mixed', 2001: 'Mixed', 2002: 'Mixed', 2003: 'Mixed', 2004: 'Mixed',
+            3: 'Asian', 3001: 'Asian', 3002: 'Asian', 3003: 'Asian', 3004: 'Asian',
+            4: 'Black', 4001: 'Black', 4002: 'Black', 4003: 'Black',
+            5: 'Chinese',
+            6: 'Other',
+            -1: 'Not known', -3: 'Not known'
+        }
+
+        gender_dict = {
+            0: 'Female',
+            1: 'Male'
+        }
+
+        demographic_df = demographic_df.replace({'Gender': gender_dict})
+        demographic_df = demographic_df.replace({'Ethnicity': ethnicity_dict})
+
+    # if using participants.tsv file
+    else:
+        demographic_df['ID'] = demographic_df['ID'].str.split('-').str[1]
+
+    demographic_df['ID'] = pd.to_numeric(demographic_df['ID'])
     demographic_df = demographic_df.dropna()
 
     id_df = pd.read_csv(id_path)
@@ -25,24 +54,5 @@ def load_demographic_data(demographic_path, id_path):
 
     # Merge supplementary demographic data with ids
     dataset = pd.merge(id_df, demographic_df, on='ID')
-
-    # Labeling data
-    ethnicity_dict = {
-        1: 'White', 1001: 'White', 1002: 'White', 1003: 'White',
-        2: 'Mixed', 2001: 'Mixed', 2002: 'Mixed', 2003: 'Mixed', 2004: 'Mixed',
-        3: 'Asian', 3001: 'Asian', 3002: 'Asian', 3003: 'Asian', 3004: 'Asian',
-        4: 'Black', 4001: 'Black', 4002: 'Black', 4003: 'Black',
-        5: 'Chinese',
-        6: 'Other',
-        -1: 'Not known', -3: 'Not known'
-    }
-
-    gender_dict = {
-        0: 'Female',
-        1: 'Male'
-    }
-
-    dataset = dataset.replace({'Gender': gender_dict})
-    dataset = dataset.replace({'Ethnicity': ethnicity_dict})
 
     return dataset
