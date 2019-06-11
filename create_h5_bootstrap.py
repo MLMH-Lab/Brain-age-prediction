2b6f057a7ce83bd3a2c4ffb6f00482e255b3ee16
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2019-06-11 21:01:31 +0100

    draft

--- create_h5_bootstrap.py
+++ create_h5_bootstrap.py
@@ -11,19 +11,17 @@ def main():
     """"""
     # ----------------------------------------------------------------------------------------
     i_n_subjects = 1
-    ids_with_n_subjects_dir = PROJECT_ROOT / 'bootstrap_analysis' / ('{:02d}'.format(i_n_subjects))
-
     n_bootstrap = 1000
+
+    demographic_path = PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner1' / 'participants.tsv'
+    ids_with_n_subjects_dir = PROJECT_ROOT / 'bootstrap_analysis' / ('{:02d}'.format(i_n_subjects))
+    freesurfer_path = PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner1' / 'freesurferData.csv'
     # ----------------------------------------------------------------------------------------
+    bootstrap_dataset_dir = ids_with_n_subjects_dir / 'datasets'
     # Loop over the 50 samples created in script create_bootstrap_ids
     for i_bootstrap in range(n_bootstrap):
         ids_filename = 'homogeneous_bootstrap_{:02d}_n_{:02d}.csv'.format(i_bootstrap, i_n_subjects)
-
-        file_name = os.path.basename(file_path)
-        print(file_name)
-        create_dataset(dataset_homogeneous=file_name,
-                       input_dir='/home/lea/PycharmProjects/predicted_brain_age/data/BIOBANK/Scanner1/bootstrap/bootstrap_ids',
-                       output_dir='/home/lea/PycharmProjects/predicted_brain_age/data/BIOBANK/Scanner1/bootstrap/h5_datasets')
+        create_dataset(demographic_path, id_path, freesurfer_path, bootstrap_dataset_dir)
 
 
 if __name__ == "__main__":
