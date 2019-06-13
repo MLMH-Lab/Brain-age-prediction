7dd01e81714953970c9d1420fec6ddaf88c80e76
Walter <walhugolp@gmail.com>
2019-06-13 11:33:20 +0100

    Fix figure plot

--- bootstrap_analysis.py
+++ bootstrap_analysis.py
@@ -15,9 +15,11 @@ def main():
     # ----------------------------------------------------------------------------------------
     experiment_dir = PROJECT_ROOT / 'outputs' / experiment_name
 
+    i_n_subjects_list = range(1, 51)
+
     scores_i_n_subjects_mean = []
     scores_i_n_subjects_std = []
-    for i_n_subjects in range(1, 51):
+    for i_n_subjects in i_n_subjects_list:
         ids_with_n_subjects_dir = experiment_dir / 'bootstrap_analysis' / ('{:02d}'.format(i_n_subjects))
         scores_dir = ids_with_n_subjects_dir / 'scores'
         scores_bootstrap = []
@@ -29,22 +31,26 @@ def main():
         scores_i_n_subjects_mean.append(np.mean(scores_bootstrap))
         scores_i_n_subjects_std.append(np.std(scores_bootstrap))
 
-    train_sizes = range(1,51)
-
-    age_min = 1
-    age_max = 40
-    std = np.sqrt(((age_max-age_min)**2)/12)
+    age_min = 47
+    age_max = 73
+    std = np.sqrt(((age_max - age_min) ** 2) / 12)
 
     # Draw lines
-    plt.plot(train_sizes, scores_i_n_subjects_mean, '--', color="#111111", label="Training score")
-    plt.plot(train_sizes, std*np.ones_like(train_sizes), color="#111111", label="Cross-validation score")
+    plt.plot(i_n_subjects_list, scores_i_n_subjects_mean, color="#111111", label="SVM performance")
+    plt.plot(i_n_subjects_list, std * np.ones_like(i_n_subjects_list), '--', color="#111111", label="Chance line")
 
     # Draw bands
-    plt.fill_between(train_sizes, scores_i_n_subjects_mean - 1.96*scores_i_n_subjects_std, scores_i_n_subjects_mean + 1.96*scores_i_n_subjects_std, color="#DDDDDD")
+    plt.fill_between(i_n_subjects_list,
+                     scores_i_n_subjects_mean - np.multiply(1.96, scores_i_n_subjects_std),
+                     scores_i_n_subjects_mean + np.multiply(1.96, scores_i_n_subjects_std),
+                     color="#DDDDDD")
 
     # Create plot
-    plt.title("Learning Curve")
-    plt.xlabel("Training Set Size"), plt.ylabel("Accuracy Score"), plt.legend(loc="best")
+    plt.title("Bootstrap Analysis")
+    plt.xlabel("Number of subjects")
+    plt.xticks(i_n_subjects_list, np.multiply(i_n_subjects_list,(73-47)))
+    plt.ylabel("Mean Absolute Error")
+    plt.legend(loc="best")
     plt.tight_layout()
     plt.show()
 
