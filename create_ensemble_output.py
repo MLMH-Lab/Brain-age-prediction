564c451a82b774d225a5b3eaaa28f67a797cdda0
Walter <walhugolp@gmail.com>
2019-06-19 13:55:13 +0100

    draft

--- create_ensemble_output.py
+++ create_ensemble_output.py
@@ -13,13 +13,22 @@ import statsmodels.api as sm
 PROJECT_ROOT = Path.cwd()
 
 
+def get_brainager(age, predicted_age):
+    """"""
+    age = sm.add_constant(age)
+    model = sm.OLS(predicted_age, age)
+    results = model.fit()
+    residuals = results.resid
+
+    return residuals
+
+
 def main():
     """"""
     # ----------------------------------------------------------------------------------------
     experiment_name = 'biobank_scanner1'
 
     n_repetitions = 10
-
     # ----------------------------------------------------------------------------------------
     experiment_dir = PROJECT_ROOT / 'outputs' / experiment_name
     svm_dir = experiment_dir / 'SVM'
@@ -29,49 +38,32 @@ def main():
     # Load SVR age predictions
     age_predictions_df = pd.read_csv(svm_dir / 'age_predictions.csv')
 
-    ensemble_df = pd.DataFrame()
-    ensemble_df['Age'] = age_predictions_df[['Age', ]]
+    ensemble_df = pd.DataFrame(age_predictions_df[['Participant_ID', 'Age']])
 
     repetition_column_name = []
     for i_repetition in range(n_repetitions):
         repetition_column_name.append('Prediction repetition {:02d}'.format(i_repetition))
 
-
-
     ensemble_df['Mean_predicted_age'] = age_predictions_df[repetition_column_name].mean(axis=1)
-    ensemble_df['Median_predicted_age'] = age_predictions_df.iloc[:, 2:last_col].median(axis=1)
-    ensemble_df['Std_predicted_age'] = age_predictions_df.iloc[:, 2:last_col].std(axis=1)
+    ensemble_df['Median_predicted_age'] = age_predictions_df[repetition_column_name].median(axis=1)
+    ensemble_df['Std_predicted_age'] = age_predictions_df[repetition_column_name].std(axis=1)
 
     # Add new columns to age_pred for age prediction error BrainAGE (Brain Age Gap Estimate)
     # BrainAGE is the difference between mean/median predicted and chronological age
-    age_predictions_df['BrainAGE_predmean'] = age_predictions_df['Mean_predicted_age'] - age_predictions_df['Age']
-    age_predictions_df['BrainAGE_predmedian'] = age_predictions_df['Median_predicted_age'] - age_predictions_df['Age']
+    ensemble_df['BrainAGE_predmean'] = ensemble_df['Mean_predicted_age'] - ensemble_df['Age']
+    ensemble_df['BrainAGE_predmedian'] = ensemble_df['Median_predicted_age'] - ensemble_df['Age']
 
     # Add new columns to age_pred for absolute BrainAGE
-    age_predictions_df['Abs_BrainAGE_predmean'] = abs(age_predictions_df['BrainAGE_predmean'])
-    age_predictions_df['Abs_BrainAGE_predmedian'] = abs(age_predictions_df['BrainAGE_predmedian'])
+    ensemble_df['Abs_BrainAGE_predmean'] = abs(ensemble_df['BrainAGE_predmean'])
+    ensemble_df['Abs_BrainAGE_predmedian'] = abs(ensemble_df['BrainAGE_predmedian'])
 
     # Add new columns to age_pred for BrainAGER (Brain Age Gap Estimate Residualized)
     # BrainAGER is a more robust measure of age prediction error (see Le et al. 2018)
-    x = age_predictions_df['Age']
-    y = age_predictions_df['Mean_predicted_age']
-    x = sm.add_constant(x)
-    brainager_model_predmean = sm.OLS(y, x)
-    brainager_results_predmean = brainager_model_predmean.fit()
-    brainager_residuals_predmean = brainager_results_predmean.resid
-    age_predictions_df['BrainAGER_predmean'] = brainager_residuals_predmean
+    age_predictions_df['BrainAGER_predmean'] = get_brainager(ensemble_df['Age'],
+                                                             ensemble_df['Mean_predicted_age'])
 
-    x = age_predictions_df['Age']
-    y = age_predictions_df['Median_predicted_age']
-    x = sm.add_constant(x)
-    brainager_model_predmedian = sm.OLS(y, x)
-    brainager_results_predmedian = brainager_model_predmedian.fit()
-    brainager_residuals_predmedian = brainager_results_predmedian.resid
-    age_predictions_df['BrainAGER_predmedian'] = brainager_residuals_predmedian
-
-    # Add new columsn to age_pred for absolute BrainAGER
-    age_predictions_df['Abs_BrainAGER_predmean'] = abs(age_predictions_df['BrainAGER_predmean'])
-    age_predictions_df['Abs_BrainAGER_predmedian'] = abs(age_predictions_df['BrainAGER_predmedian'])
+    age_predictions_df['BrainAGER_predmedian'] = get_brainager(ensemble_df['Age'],
+                                                               ensemble_df['Median_predicted_age'])
 
 
 if __name__ == "__main__":
