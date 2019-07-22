f2e8846db65a10d151cd908743c28845f7ed85c9
Lea <k1802521@kcl.ac.uk>
2019-07-22 17:17:25 +0100

    changed i_n_subjects var name to n_gender_pairs to make the name more descriptive

--- create_h5_bootstrap.py
+++ create_h5_bootstrap.py
@@ -12,7 +12,7 @@ def main():
     # ----------------------------------------------------------------------------------------
     experiment_name = 'biobank_scanner1'
 
-    i_n_subjects = 1
+    n_gender_pairs = 1
     n_bootstrap = 1000
 
     demographic_path = PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner1' / 'participants.tsv'
@@ -20,17 +20,17 @@ def main():
     # ----------------------------------------------------------------------------------------
     experiment_dir = PROJECT_ROOT / 'outputs' / experiment_name
 
-    ids_with_n_subjects_dir = experiment_dir / 'bootstrap_analysis' / ('{:02d}'.format(i_n_subjects))
+    ids_with_n_subjects_dir = experiment_dir / 'bootstrap_analysis' / ('{:02d}'.format(n_gender_pairs))
 
     dataset_dir = ids_with_n_subjects_dir / 'datasets'
     dataset_dir.mkdir(exist_ok=True)
 
     # Loop over the 50 samples created in script create_bootstrap_ids
     for i_bootstrap in range(n_bootstrap):
-        ids_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}.csv'.format(i_bootstrap, i_n_subjects)
+        ids_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}.csv'.format(i_bootstrap, n_gender_pairs)
         id_path = ids_with_n_subjects_dir / 'ids' / ids_filename
 
-        dataset_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}.h5'.format(i_bootstrap, i_n_subjects)
+        dataset_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}.h5'.format(i_bootstrap, n_gender_pairs)
         dataset_path = dataset_dir / dataset_filename
 
         create_dataset(demographic_path, id_path, freesurfer_path, dataset_path)
