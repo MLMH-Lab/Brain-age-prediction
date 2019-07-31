81c564c7c53e78c19926ebec9a9606c0db5d6bd7
Lea <k1802521@kcl.ac.uk>
2019-07-31 15:25:14 +0100

    added loop over all bootstrap samples, amended variable names and added comments

--- create_h5_bootstrap.py
+++ create_h5_bootstrap.py
@@ -1,5 +1,7 @@
-"""Script to create bootstrap datasets of UK BIOBANK Scanner1
-in hdf5 format using create_h5_dataset script"""
+"""Script to create bootstrap datasets of UK BIOBANK Scanner1 in hdf5 format using create_h5_dataset script;
+Uses ids from create_bootstrap_ids
+"""
+
 from pathlib import Path
 
 from create_h5_dataset import create_dataset
@@ -11,29 +13,31 @@ def main():
     """"""
     # ----------------------------------------------------------------------------------------
     experiment_name = 'biobank_scanner1'
-
-    n_gender_pairs = 1
-    n_bootstrap = 1000
-
     demographic_path = PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner1' / 'participants.tsv'
     freesurfer_path = PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner1' / 'freesurferData.csv'
-    # ----------------------------------------------------------------------------------------
+
     experiment_dir = PROJECT_ROOT / 'outputs' / experiment_name
+    # ----------------------------------------------------------------------------------------
 
-    ids_with_n_subjects_dir = experiment_dir / 'bootstrap_analysis' / ('{:02d}'.format(n_gender_pairs))
+    # Loop over the 50 bootstrap samples with up to 50 gender-balanced subject pairs per age group/year
+    for i_n_subject_pairs in range(1, 51):
+        print(i_n_subject_pairs)
 
-    dataset_dir = ids_with_n_subjects_dir / 'datasets'
-    dataset_dir.mkdir(exist_ok=True)
+        ids_with_n_subject_pairs_dir = experiment_dir / 'bootstrap_analysis' / ('{:02d}'.format(i_n_subject_pairs))
 
-    # Loop over the 50 samples created in script create_bootstrap_ids
-    for i_bootstrap in range(n_bootstrap):
-        ids_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}.csv'.format(i_bootstrap, n_gender_pairs)
-        id_path = ids_with_n_subjects_dir / 'ids' / ids_filename
+        dataset_dir = ids_with_n_subject_pairs_dir / 'datasets'
+        dataset_dir.mkdir(exist_ok=True)
 
-        dataset_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}.h5'.format(i_bootstrap, n_gender_pairs)
-        dataset_path = dataset_dir / dataset_filename
+        # Loop over the 1000 random subject samples per bootstrap
+        n_bootstrap = 1000
+        for i_bootstrap in range(n_bootstrap):
+            ids_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}.csv'.format(i_bootstrap, i_n_subject_pairs)
+            id_path = ids_with_n_subject_pairs_dir / 'ids' / ids_filename
 
-        create_dataset(demographic_path, id_path, freesurfer_path, dataset_path)
+            dataset_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}.h5'.format(i_bootstrap, i_n_subject_pairs)
+            dataset_path = dataset_dir / dataset_filename
+
+            create_dataset(demographic_path, id_path, freesurfer_path, dataset_path)
 
 
 if __name__ == "__main__":
