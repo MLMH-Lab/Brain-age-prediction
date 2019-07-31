39ebdd9ee3a4539d174ecec4f5a7e24c19b5f49b
Lea <k1802521@kcl.ac.uk>
2019-07-31 17:54:56 +0100

    changed var names from subjects to subject_pairs to align with other scripts

--- bootstrap_analysis.py
+++ bootstrap_analysis.py
@@ -1,4 +1,5 @@
-""""""
+"""Plot results of bootstrap analysis"""
+
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -10,45 +11,46 @@ PROJECT_ROOT = Path.cwd()
 def main():
     # ----------------------------------------------------------------------------------------
     experiment_name = 'biobank_scanner1'
-
-    n_bootstrap = 1000
-    # ----------------------------------------------------------------------------------------
     experiment_dir = PROJECT_ROOT / 'outputs' / experiment_name
+    # ----------------------------------------------------------------------------------------
 
-    i_n_subjects_list = range(1, 51)
+    i_n_subject_pairs_list = range(1, 51)
+    # n_bootstrap = 1000
+    n_bootstrap = 20
 
-    scores_i_n_subjects_mean = []
-    scores_i_n_subjects_std = []
-    for i_n_subjects in i_n_subjects_list:
-        ids_with_n_subjects_dir = experiment_dir / 'bootstrap_analysis' / ('{:02d}'.format(i_n_subjects))
-        scores_dir = ids_with_n_subjects_dir / 'scores'
+    scores_i_n_subject_pairs_mean = []
+    scores_i_n_subject_pairs_std = []
+
+    for i_n_subject_pairs in i_n_subject_pairs_list:
+        ids_with_n_subject_pairs_dir = experiment_dir / 'bootstrap_analysis' / ('{:02d}'.format(i_n_subject_pairs))
+        scores_dir = ids_with_n_subject_pairs_dir / 'scores' #TODO: should this loop over scores and scores_classifier or compare them in some way?
         scores_bootstrap = []
         for i_bootstrap in range(n_bootstrap):
             # Save arrays with permutation coefs and scores as np files
             filepath_scores = scores_dir / ('boot_scores_{:04d}.npy'.format(i_bootstrap))
             scores_bootstrap.append(np.load(str(filepath_scores))[1])
 
-        scores_i_n_subjects_mean.append(np.mean(scores_bootstrap))
-        scores_i_n_subjects_std.append(np.std(scores_bootstrap))
+        scores_i_n_subject_pairs_mean.append(np.mean(scores_bootstrap))
+        scores_i_n_subject_pairs_std.append(np.std(scores_bootstrap))
 
     age_min = 47
     age_max = 73
     std = np.sqrt(((age_max - age_min) ** 2) / 12)
 
     # Draw lines
-    plt.plot(i_n_subjects_list, scores_i_n_subjects_mean, color="#111111", label="SVM performance")
-    plt.plot(i_n_subjects_list, std * np.ones_like(i_n_subjects_list), '--', color="#111111", label="Chance line")
+    plt.plot(i_n_subject_pairs_list, scores_i_n_subject_pairs_mean, color="#111111", label="SVM performance")
+    plt.plot(i_n_subject_pairs_list, std * np.ones_like(i_n_subject_pairs_list), '--', color="#111111", label="Chance line")
 
     # Draw bands
-    plt.fill_between(i_n_subjects_list,
-                     scores_i_n_subjects_mean - np.multiply(1.96, scores_i_n_subjects_std),
-                     scores_i_n_subjects_mean + np.multiply(1.96, scores_i_n_subjects_std),
+    plt.fill_between(i_n_subject_pairs_list,
+                     scores_i_n_subject_pairs_mean - np.multiply(1.96, scores_i_n_subject_pairs_std),
+                     scores_i_n_subject_pairs_mean + np.multiply(1.96, scores_i_n_subject_pairs_std),
                      color="#DDDDDD")
 
     # Create plot
     plt.title("Bootstrap Analysis")
     plt.xlabel("Number of subjects")
-    plt.xticks(i_n_subjects_list, np.multiply(i_n_subjects_list,(73-47)))
+    plt.xticks(i_n_subject_pairs_list, np.multiply(i_n_subject_pairs_list,(73-47)))
     plt.ylabel("Mean Absolute Error")
     plt.legend(loc="best")
     plt.tight_layout()
