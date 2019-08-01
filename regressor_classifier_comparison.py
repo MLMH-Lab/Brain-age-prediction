7e1e116b38f45d9747b0e7b590540aa51f13cde2
Walter <walhugolp@gmail.com>
2019-08-01 14:19:33 +0100

    Fix classifier vs regressor analysis

--- regressor_classifier_comparison.py
+++ regressor_classifier_comparison.py
@@ -1,74 +1,29 @@
-""""""
+"""Script to compare the CORR score (correlation between the prediction error and the subject age) from a
+classifier and from a regressor (using an uniform sample with 50 pairs)."""
 from pathlib import Path
 
-import pandas as pd
 import numpy as np
 from scipy import stats
 
+from regressors_comparison import ttest_ind_corrected
+
 PROJECT_ROOT = Path.cwd()
-
-
-
-def ttest_ind_corrected(a, b, k=10, r=10):
-    """
-        Corrected repeated k-fold cv test
-
-                The test assumes that the classifiers were evaluated using cross
-    validation. The number of folds is determined from the length of the vector
-    of differences, as `len(diff) / runs`. The variance includes a correction
-    for underestimation of variance due to overlapping training sets, as
-    described in `Inference for the Generalization Error
-    <http://link.springer.com/article/10.1023%2FA%3A1024068626366>`_,
-    C. Nadeau and Y. Bengio, Mach Learning 2003.)
-
-
-    n1 is the number of instances used  for training, and n2 the number of instances used for testing
-
-    n2 / n1 = 1/ (nfolds-1) = 1/(k-1)
-    r-times k-fold cross-validations there are r,r>1, runs and k,k>1, folds.
-
-    Ref:
-        Bouckaert, Remco R., and Eibe Frank. "Evaluating the replicability of significance tests for comparing learning algorithms." Pacific-Asia Conference on Knowledge Discovery and Data Mining. Springer, Berlin, Heidelberg, 2004.
-
-
-    Args:
-        a: performances from classifier A
-        b: performances from classifier A
-        k: number of folds
-        r: number of repetitions
-
-    Returns:
-
-    """
-    df = k * r - 1
-
-    x = a - b
-    m = np.mean(x)
-
-    sigma_2 = np.var(x, ddof=1)
-    denom = np.sqrt((1 / k * r + 1 / (k - 1)) * sigma_2)
-
-    with np.errstate(divide='ignore', invalid='ignore'):
-        t = np.divide(m, denom)
-
-    prob = stats.t.sf(np.abs(t), df) * 2
-
-    return t, prob
 
 
 def main():
     # ----------------------------------------------------------------------------------------
     experiment_name = 'biobank_scanner1'
 
-    i_n_subjects = 50
-    n_bootstrap = 500
     # ----------------------------------------------------------------------------------------
     experiment_dir = PROJECT_ROOT / 'outputs' / experiment_name
 
+    i_n_subjects = 50
     ids_with_n_subjects_dir = experiment_dir / 'bootstrap_analysis' / ('{:02d}'.format(i_n_subjects))
 
     scores_classifier_dir = ids_with_n_subjects_dir / 'scores_classifier'
     scores_regressor_dir = ids_with_n_subjects_dir / 'scores'
+
+    n_bootstrap = 1000
 
     regressor_corr_list = []
     classifier_corr_list = []
@@ -78,10 +33,13 @@ def main():
         regressor_scores = np.load(str(scores_regressor_dir / scores_filename))
         classifier_scores = np.load(str(scores_classifier_dir / scores_filename))
 
+        # Store CORR score (position 3 in list of scores)
         regressor_corr_list.append(regressor_scores[3])
         classifier_corr_list.append(classifier_scores[3])
 
+    # Check if CORR score is significantly different between regressor and classifier.
     stat, pvalue = ttest_ind_corrected(np.array(regressor_corr_list), np.array(classifier_corr_list))
+    print('CORR from classifier vs. CORR from regressor - pvalue: {:6.3}'.format(pvalue))
 
 
 if __name__ == "__main__":
