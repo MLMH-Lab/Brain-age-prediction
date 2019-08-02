41d4e030ef61748bd3ef693ebfbfbbb9ae91457d
Lea <k1802521@kcl.ac.uk>
2019-08-02 16:59:08 +0100

    Output of analyses saved as dataframes

--- var_corr.py
+++ var_corr.py
@@ -59,22 +59,57 @@ def main():
     dataset = pd.merge(ensemble_df, variables_df, on='ID')
 
     # Correlation variables
-    y_list = ['BrainAGE_predmean', 'BrainAGE_predmedian',
-              'BrainAGER_predmean', 'BrainAGER_predmedian',
-              'Std_predicted_age']
+    y_list = ['BrainAGE_predmean', 'BrainAGER_predmean']
     x_list = ['Air_pollution',
-              'Traffic_intensity', 'Inverse_dist_road', 'Close_road_bin',
+              'Traffic_intensity', 'Inverse_dist_road',
               'Greenspace_perc', 'Garden_perc', 'Water_perc', 'Natural_env_perc']
 
-    # Spearman's correlation per variable
-    print("Spearman's correlation")
+    # Create empty dataframe to store correlation results
+    corr_output = pd.DataFrame({'Row_labels_1': ['Air_pollution', 'Air_pollution', 'Air_pollution',
+                                                    'Traffic_intensity', 'Traffic_intensity', 'Traffic_intensity',
+                                                    'Inverse_dist_road', 'Inverse_dist_road', 'Inverse_dist_road',
+                                                    'Greenspace_perc', 'Greenspace_perc', 'Greenspace_perc',
+                                                    'Garden_perc', 'Garden_perc', 'Garden_perc',
+                                                    'Water_perc', 'Water_perc', 'Water_perc',
+                                                    'Natural_env_perc', 'Natural_env_perc', 'Natural_env_perc'],
+                               'Row_labels_2': ['n', 'p_val', 'rho',
+                                                   'n', 'p_val', 'rho',
+                                                   'n', 'p_val', 'rho',
+                                                   'n', 'p_val', 'rho',
+                                                   'n', 'p_val', 'rho',
+                                                   'n', 'p_val', 'rho',
+                                                   'n', 'p_val', 'rho']})
+    corr_output.set_index('Row_labels_1', 'Row_labels_2')
+
+    # Spearman's correlation per variable, add to corr_output
     for y in y_list:
+        y_values = []
         for x in x_list:
             dataset_x = dataset.dropna(subset=[x])
-            print(y, x, len(dataset_x))
-            spearman(dataset_x, x, y)
+            n = len(dataset_x)
+            spearman_rho, spearman_p = spearmanr(dataset_x[x], dataset_x[y])
+            y_values.append(n)
+            y_values.append(spearman_p)
+            y_values.append(spearman_rho)
+        corr_output[y] = y_values
 
-    # Analysis of education level
+    corr_output.to_csv(correlation_dir / 'variables_biobank_spearman_output.csv')
+
+    # Create empty dataframe for analysis of education level
+    education_output = pd.DataFrame({'Row_labels_1': ['uni vs prof_qual', 'uni vs prof_qual',
+                                                      'uni vs a_level', 'uni vs a_level',
+                                                      'uni vs gcse', 'uni vs gcse',
+                                                      'prof_qual vs a_level', 'prof_qual vs a_level',
+                                                      'prof_qual vs gcse', 'prof_qual vs gcse',
+                                                      'a_level vs gcse', 'a_level vs gcse'],
+                                     'Row_labels_2': ['p_val', 'rho',
+                                                      'p_val', 'rho',
+                                                      'p_val', 'rho',
+                                                      'p_val', 'rho',
+                                                      'p_val', 'rho',
+                                                      'p_val', 'rho']})
+    education_output.set_index('Row_labels_1', 'Row_labels_2')
+
     dataset_uni = dataset.groupby('Education_highest').get_group(uni_code)
     dataset_prof_qual = dataset.groupby('Education_highest').get_group(prof_qual_code)
     dataset_a_level = dataset.groupby('Education_highest').get_group(a_level_code)
@@ -84,38 +119,54 @@ def main():
     alpha_corrected = 0.05 / 6
 
     for y in y_list:
+        y_results = []
 
         print('\n', y)
         tstat, pval = ttest_ind(dataset_uni[y], dataset_prof_qual[y])
         effect_size = cohend(dataset_uni[y], dataset_prof_qual[y])
+        y_results.append(pval)
+        y_results.append(effect_size)
         if pval < alpha_corrected:
             print("uni vs prof_qual [t-test pval, cohen's d]", pval, effect_size)
 
         tstat, pval = ttest_ind(dataset_uni[y], dataset_a_level[y])
         effect_size = cohend(dataset_uni[y], dataset_a_level[y])
+        y_results.append(pval)
+        y_results.append(effect_size)
         if pval < alpha_corrected:
             print("uni vs a_level [t-test pval, cohen's d]", pval, effect_size)
 
         tstat, pval = ttest_ind(dataset_uni[y], dataset_gcse[y])
         effect_size = cohend(dataset_uni[y], dataset_gcse[y])
+        y_results.append(pval)
+        y_results.append(effect_size)
         if pval < alpha_corrected:
             print("uni vs gcse [t-test pval, cohen's d]", pval, effect_size)
 
         tstat, pval = ttest_ind(dataset_prof_qual[y], dataset_a_level[y])
         effect_size = cohend(dataset_prof_qual[y], dataset_a_level[y])
+        y_results.append(pval)
+        y_results.append(effect_size)
         if pval < alpha_corrected:
             print("prof_qual vs a_level [t-test pval, cohen's d]", pval, effect_size)
 
         tstat, pval = ttest_ind(dataset_prof_qual[y], dataset_gcse[y])
         effect_size = cohend(dataset_prof_qual[y], dataset_gcse[y])
+        y_results.append(pval)
+        y_results.append(effect_size)
         if pval < alpha_corrected:
             print("prof_qual vs gcse [t-test pval, cohen's d]", pval, effect_size)
 
         tstat, pval = ttest_ind(dataset_a_level[y], dataset_gcse[y])
         effect_size = cohend(dataset_a_level[y], dataset_gcse[y])
+        y_results.append(pval)
+        y_results.append(effect_size)
         if pval < alpha_corrected:
             print("a_level vs gcse [t-test pval, cohen's d]", pval, effect_size)
 
+        education_output[y] = y_results
+
+    education_output.to_csv(correlation_dir / 'education_ttest_output.csv')
 
 if __name__ == "__main__":
     main()
