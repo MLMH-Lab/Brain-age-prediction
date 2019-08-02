122029f6be9d9bf78a7f4f99ee0386e7220fcde0
Lea <k1802521@kcl.ac.uk>
2019-08-02 16:07:59 +0100

    Tried to simplify var_corr script - saving output still to do

--- var_corr.py
+++ var_corr.py
@@ -5,10 +5,7 @@ from pathlib import Path
 
 import pandas as pd
 import numpy as np
-from scipy.stats import spearmanr, f_oneway, ttest_ind
-from statsmodels.stats.multicomp import MultiComparison
-import statsmodels.api as sm
-import matplotlib.pyplot as plt
+from scipy.stats import spearmanr, ttest_ind
 
 PROJECT_ROOT = Path.cwd()
 
@@ -19,31 +16,31 @@ gcse_code = 1
 
 
 def spearman(df, x, y):
-    """Calculate and interpret spearman's correlation of cols x and y"""
+    """Calculate and interpret Spearman's rank-order correlation of variables x and y"""
 
     spearman_rho, spearman_p = spearmanr(df[x], df[y])
 
     alpha = 0.05
     n = len(df)
+
     if spearman_p < alpha:
         print('n=%s, %s and %s - reject H0: p = %.3f, rho = %.3f'
+              % (n, x, y, spearman_p, spearman_rho))
+    else:
+        print('n=%s, %s and %s - not significant: p = %.3f, rho = %.3f'
               % (n, x, y, spearman_p, spearman_rho))
 
 
 def cohend(d1, d2):
-    """Function to calculate Cohen's d for independent samples"""
+    """Calculate Cohen's d effect size for independent samples"""
 
-    # calculate the size of samples
     n1, n2 = len(d1), len(d2)
-    # calculate the variance of the samples
-    s1, s2 = np.var(d1, ddof=1), np.var(d2, ddof=1)
-    # calculate the pooled standard deviation
-    s = np.sqrt(((n1 - 1) * s1 + (n2 - 1) * s2) / (n1 + n2 - 2))
-    # calculate the means of the samples
-    u1, u2 = np.mean(d1), np.mean(d2)
-    # calculate the effect size
-    effect_size = (u1 - u2) / s
-    print(d1, d2, effect_size, '\n')
+    d1_variance, d2_variance = np.var(d1, ddof=1), np.var(d2, ddof=1)
+    std_pooled = np.sqrt(((n1 - 1) * d1_variance + (n2 - 1) * d2_variance) / (n1 + n2 - 2))
+    d1_mean, d2_mean = np.mean(d1), np.mean(d2)
+    effect_size = (d1_mean - d2_mean) / std_pooled
+
+    return effect_size
 
 
 def main():
@@ -65,64 +62,59 @@ def main():
     y_list = ['BrainAGE_predmean', 'BrainAGE_predmedian',
               'BrainAGER_predmean', 'BrainAGER_predmedian',
               'Std_predicted_age']
-
     x_list = ['Air_pollution',
               'Traffic_intensity', 'Inverse_dist_road', 'Close_road_bin',
               'Greenspace_perc', 'Garden_perc', 'Water_perc', 'Natural_env_perc']
 
-    # Spearman correlation per variable
-    print("Spearman correlation")
+    # Spearman's correlation per variable
+    print("Spearman's correlation")
     for y in y_list:
         for x in x_list:
-            dataset_y = dataset.dropna(subset=[y])
-            spearman(dataset_y, x, y)
+            dataset_x = dataset.dropna(subset=[x])
+            print(y, x, len(dataset_x))
+            spearman(dataset_x, x, y)
 
-
+    # Analysis of education level
     dataset_uni = dataset.groupby('Education_highest').get_group(uni_code)
     dataset_prof_qual = dataset.groupby('Education_highest').get_group(prof_qual_code)
     dataset_a_level = dataset.groupby('Education_highest').get_group(a_level_code)
     dataset_gcse = dataset.groupby('Education_highest').get_group(gcse_code)
 
+    # Independent t-tests with alpha corrected for multiple comparisons using Bonferroni's method
+    alpha_corrected = 0.05 / 6
 
-    # bonferroni-corrected alpha for multiple t-tests
-    alpha_bon = 0.05 / 6
+    for y in y_list:
 
-    for x in x_list:
-        plist = []
-        print('\n', x)
-        tstat, pval = ttest_ind(dataset_uni[x], dataset_prof_qual[x])
-        plist.append(pval)
-        if pval < alpha_bon:
-            print("uni vs prof_qual", pval)
-        tstat, pval = ttest_ind(dataset_uni[x], dataset_a_level[x])
-        plist.append(pval)
-        if pval < alpha_bon:
-            print("uni vs a_level", pval)
-        tstat, pval = ttest_ind(dataset_uni[x], dataset_gcse[x])
-        plist.append(pval)
-        if pval < alpha_bon:
-            print("uni vs gcse", pval)
-        tstat, pval = ttest_ind(dataset_prof_qual[x], dataset_a_level[x])
-        plist.append(pval)
-        if pval < alpha_bon:
-            print("prof_qual vs a_level", pval)
-        tstat, pval = ttest_ind(dataset_prof_qual[x], dataset_gcse[x])
-        plist.append(pval)
-        if pval < alpha_bon:
-            print("prof_qual vs gcse", pval)
-        tstat, pval = ttest_ind(dataset_a_level[x], dataset_gcse[x])
-        plist.append(pval)
-        if pval < alpha_bon:
-            print("a_level vs gcse", pval)
-        # print(multipletests(plist, alpha=0.05, method='bonferroni'))
+        print('\n', y)
+        tstat, pval = ttest_ind(dataset_uni[y], dataset_prof_qual[y])
+        effect_size = cohend(dataset_uni[y], dataset_prof_qual[y])
+        if pval < alpha_corrected:
+            print("uni vs prof_qual [t-test pval, cohen's d]", pval, effect_size)
 
-    # Cohen's d test for education levels # outputs for all vars
-    cohend(dataset_uni, dataset_prof_qual)
-    cohend(dataset_uni, dataset_a_level)
-    cohend(dataset_uni, dataset_gcse)
-    cohend(dataset_prof_qual, dataset_a_level)
-    cohend(dataset_prof_qual, dataset_gcse)
-    cohend(dataset_a_level, dataset_gcse)
+        tstat, pval = ttest_ind(dataset_uni[y], dataset_a_level[y])
+        effect_size = cohend(dataset_uni[y], dataset_a_level[y])
+        if pval < alpha_corrected:
+            print("uni vs a_level [t-test pval, cohen's d]", pval, effect_size)
+
+        tstat, pval = ttest_ind(dataset_uni[y], dataset_gcse[y])
+        effect_size = cohend(dataset_uni[y], dataset_gcse[y])
+        if pval < alpha_corrected:
+            print("uni vs gcse [t-test pval, cohen's d]", pval, effect_size)
+
+        tstat, pval = ttest_ind(dataset_prof_qual[y], dataset_a_level[y])
+        effect_size = cohend(dataset_prof_qual[y], dataset_a_level[y])
+        if pval < alpha_corrected:
+            print("prof_qual vs a_level [t-test pval, cohen's d]", pval, effect_size)
+
+        tstat, pval = ttest_ind(dataset_prof_qual[y], dataset_gcse[y])
+        effect_size = cohend(dataset_prof_qual[y], dataset_gcse[y])
+        if pval < alpha_corrected:
+            print("prof_qual vs gcse [t-test pval, cohen's d]", pval, effect_size)
+
+        tstat, pval = ttest_ind(dataset_a_level[y], dataset_gcse[y])
+        effect_size = cohend(dataset_a_level[y], dataset_gcse[y])
+        if pval < alpha_corrected:
+            print("a_level vs gcse [t-test pval, cohen's d]", pval, effect_size)
 
 
 if __name__ == "__main__":
