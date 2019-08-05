739248fb6ec747c9dd73114bd9af0c718d4e6c22
Lea <k1802521@kcl.ac.uk>
2019-08-05 15:03:03 +0100

    Updated and simplified script

--- src/BIOBANK/Scanner1/lsoa_corr.py
+++ src/BIOBANK/Scanner1/lsoa_corr.py
@@ -1,78 +1,92 @@
 """Script to assess correlations between age prediction error
-and English index of multiple deprivation (IMD) variables"""
+and English indices of deprivation [1]
+
+References:
+[1] Department for Communities and Local Government. English Indices of Deprivation 2015. 2015.
+https://www.gov.uk/government/statistics/english-indices-of-deprivation-2015
+"""
 
 from pathlib import Path
 
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
-import matplotlib.pyplot as plt
 
-PROJECT_ROOT = Path('/home/lea/PycharmProjects/predicted_brain_age')
+PROJECT_ROOT = Path.cwd()
 
 
-def ols_reg(df, indep, dep):
+def ols_reg(df, x, y):
     """Perform linear regression using ordinary least squares (OLS) method"""
 
-    endog = np.asarray(df[indep], dtype=float)
-    exog = np.asarray(sm.add_constant(df[dep]), dtype=float)
-    OLS_model = sm.OLS(endog, exog)
+    y = np.asarray(df[y], dtype=float)
+    x = np.asarray(sm.add_constant(df[x]), dtype=float)
+    OLS_model = sm.OLS(y, x)
     OLS_results = OLS_model.fit()
     OLS_p = OLS_results.pvalues[1]
     OLS_coeff = OLS_results.params[1]
 
-    alpha = 0.05
-    n = len(df)
-    if OLS_p < alpha:
-        print('n=%s, %s and %s - reject H0: p = %.3f, coef = %.3f'
-              % (n, indep, dep, OLS_p, OLS_coeff))
-    # elif OLS_p >= alpha:
-    #     print('%s and %s - fail to reject H0: p = %.3f, coef = %.3f'
-    #           % (indep, dep, OLS_p, OLS_coeff))
-    # else:
-    #     print('Error with %s and %s' % (indep, dep))
+    return OLS_p, OLS_coeff
 
 
 def main():
-    # Define what subjects dataset should contain: total, male or female
-    subjects = 'total'
+    """"""
+    # ----------------------------------------------------------------------------------------
+    experiment_name = 'biobank_scanner1'
+    # ----------------------------------------------------------------------------------------
+    correlation_dir = PROJECT_ROOT / 'outputs' / experiment_name / 'correlation_analysis'
 
-    # Load dataset with age vars, demographic data from Biobank, demographic data from IMD
-    dataset = pd.read_csv(str(PROJECT_ROOT / 'outputs' / subjects / 'age_predictions_demographics.csv'))
+    ensemble_df = pd.read_csv(correlation_dir / 'ensemble_output.csv')
+    variables_indices_df = pd.read_csv(correlation_dir / 'variables_indices_deprivation.csv')
+    dataset = pd.merge(ensemble_df, variables_indices_df, on='Participant_ID')
 
-    # create list of IMD vars, excluding supplementary children and elderly measures
-    col = ['IMD_decile', 'IMD_rank', 'IMD_score',
-           'Income_deprivation_decile', 'Income_deprivation_rank', 'Income_deprivation_score',
-           'Employment_deprivation_decile', 'Employment_deprivation_rank', 'Employment_deprivation_score',
-           'Education_deprivation_decile', 'Education_deprivation_rank', 'Education_deprivation_score',
-           'Health_deprivation_decile', 'Health_deprivation_rank', 'Health_deprivation_score',
-           'Crime_decile', 'Crime_rank', 'Crime_score',
-           'Barrier_housing_decile', 'Barrier_housing_rank', 'Barrier_housing_score',
-           'Environment_deprivation_decile', 'Environment_deprivation_rank', 'Environment_deprivation_score']
+    # Regression variables
+    # Indices of deprivation variables exclude supplementary children and elderly measures
+    y_list = ['BrainAGE_predmean', 'BrainAGER_predmean']
+    x_list = ['IMD_decile', 'IMD_rank', 'IMD_score',
+              'Income_deprivation_decile', 'Income_deprivation_rank', 'Income_deprivation_score',
+              'Employment_deprivation_decile', 'Employment_deprivation_rank', 'Employment_deprivation_score',
+              'Education_deprivation_decile', 'Education_deprivation_rank', 'Education_deprivation_score',
+              'Health_deprivation_decile', 'Health_deprivation_rank', 'Health_deprivation_score',
+              'Crime_decile', 'Crime_rank', 'Crime_score',
+              'Barrier_housing_decile', 'Barrier_housing_rank', 'Barrier_housing_score',
+              'Environment_deprivation_decile', 'Environment_deprivation_rank', 'Environment_deprivation_score']
 
-    # Correlation variables
-    # x_list = ['Abs_BrainAGE_predmean', 'Abs_BrainAGE_predmedian',
-    #           'Abs_BrainAGER_predmean', 'Abs_BrainAGER_predmedian',
-    #           'BrainAGE_predmean', 'BrainAGE_predmean',
-    #           'BrainAGER_predmean', 'BrainAGER_predmedian',
-    #           'Std_predicted_age']
-
-    # # Shortened x_list
-    x_list = ['Abs_BrainAGER_predmean', 'BrainAGER_predmean']
+    # Create empty dataframe for analysis of indices of deprivation
+    indices_output = pd.DataFrame({'Row_labels_1': ['IMD_score', 'IMD_score',
+                                                    'Income_deprivation_score', 'Income_deprivation_score',
+                                                    'Employment_deprivation_score', 'Employment_deprivation_score',
+                                                    'Education_deprivation_score', 'Education_deprivation_score',
+                                                    'Health_deprivation_score', 'Health_deprivation_score',
+                                                    'Crime_score', 'Crime_score',
+                                                    'Barrier_housing_score', 'Barrier_housing_score',
+                                                    'Environment_deprivation_score', 'Environment_deprivation_score'],
+                                   'Row_labels_2': ['p_val', 'coef',
+                                                    'p_val', 'coef',
+                                                    'p_val', 'coef',
+                                                    'p_val', 'coef',
+                                                    'p_val', 'coef',
+                                                    'p_val', 'coef',
+                                                    'p_val', 'coef',
+                                                    'p_val', 'coef']})
+    indices_output.set_index('Row_labels_1', 'Row_labels_2')
 
     # create list of deprivation vars with scores only
-    col_score = []
-    for item in col:
+    x_score = []
+    for item in x_list:
         if item.split('_')[-1] == 'score':
-            col_score.append(item)
+            x_score.append(item)
 
-    # Linear regression per IMD var
-    for var in col_score:
-        for x in x_list:
-            dataset_var = dataset.dropna(subset=[var])
-            ols_reg(dataset_var, x, var)
+    # Perform linear regression
+    for y in y_list:
+        y_results = []
+        for x in x_score:
+            dataset_var = dataset.dropna(subset=[x])
+            OLS_p, OLS_coeff = ols_reg(dataset_var, y, x)
+            y_results.append(OLS_p)
+            y_results.append(OLS_coeff)
+        indices_output[y] = y_results
 
-    # TODO: bonferroni correction?
+    indices_output.to_csv(correlation_dir / 'indices_deprivation_ols_output.csv')
 
 
 if __name__ == "__main__":
