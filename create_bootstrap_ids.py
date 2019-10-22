b890a26293e9e9debf23aab92aff6f690f8ed9a2
Warvito <ianonimato@hotmail.com>
2019-10-22 10:33:29 +0100

    Implement out of bag bootstrap

--- create_bootstrap_ids.py
+++ create_bootstrap_ids.py
@@ -1,5 +1,5 @@
 """
-Script to create gender-homogeneous bootstrap datasets to feed into create_h5_bootstrap script;
+Script to create gender-homogeneous bootstraped datasets to feed into create_h5_bootstrap script;
 Creates 50 bootstrap samples with increasing size
 """
 from pathlib import Path
@@ -34,8 +34,11 @@ def main():
     age_min = int(dataset['Age'].min())  # 47
     age_max = int(dataset['Age'].max())  # 73
 
+    # Number maximum of pairs
+    n_max_pair = 50
     # Loop to create 50 bootstrap samples that each contain up to 50 gender-balanced subject pairs per age group/year
-    for i_n_subject_pairs in range(1, 51):
+    # For the smallest number of pair, create a out-of-bag set (~test set) with same size of training set
+    for i_n_subject_pairs in range(1, n_max_pair//2):
         print(i_n_subject_pairs)
         ids_with_n_subject_pairs_dir = bootstrap_dir / '{:02d}'.format(i_n_subject_pairs)
         ids_with_n_subject_pairs_dir.mkdir(exist_ok=True)
@@ -46,7 +49,8 @@ def main():
         n_bootstrap = 1000
         for i_bootstrap in range(n_bootstrap):
             # Create empty df to add bootstrap subjects to
-            dataset_bootstrap = pd.DataFrame(columns=['Participant_ID'])
+            dataset_bootstrap_train = pd.DataFrame(columns=['Participant_ID'])
+            dataset_bootstrap_test = pd.DataFrame(columns=['Participant_ID'])
 
             # Loop over ages (27 in total)
             for age in range(age_min, (age_max + 1)):
@@ -58,13 +62,56 @@ def main():
                 for gender in range(2):
                     gender_group = age_group.groupby('Gender').get_group(gender)
 
-                    # Extract random subject of that gender and add to dataset_bootstrap
-                    random_row = gender_group.sample(n=i_n_subject_pairs, replace=True)
-                    dataset_bootstrap = pd.concat([dataset_bootstrap, pd.DataFrame(random_row['Participant_ID'])])
+                    # Extract random subject of that gender and add to dataset_bootstrap_train
+                    random_sample_train = gender_group.sample(n=i_n_subject_pairs, replace=True)
+                    dataset_bootstrap_train = pd.concat([dataset_bootstrap_train, pd.DataFrame(random_sample_train['Participant_ID'])])
 
-            # Export dataset_bootstrap as csv
+                    not_sampled = ~gender_group['Participant_ID'].isin(random_sample_train['Participant_ID'])
+                    random_sample_test = gender_group[not_sampled].sample(n=i_n_subject_pairs, replace=False)
+                    dataset_bootstrap_test = pd.concat([dataset_bootstrap_test, pd.DataFrame(random_sample_test['Participant_ID'])])
+
+            # Export dataset_bootstrap_train as csv
+            ids_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}_train.csv'.format(i_bootstrap, i_n_subject_pairs)
+            dataset_bootstrap_train.to_csv(ids_dir / ids_filename, index=False)
+
+            ids_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}_test.csv'.format(i_bootstrap, i_n_subject_pairs)
+            dataset_bootstrap_test.to_csv(ids_dir / ids_filename, index=False)
+
+
+    for i_n_subject_pairs in range((n_max_pair//2) + 1, n_max_pair+1):
+        print(i_n_subject_pairs)
+        ids_with_n_subject_pairs_dir = bootstrap_dir / '{:02d}'.format(i_n_subject_pairs)
+        ids_with_n_subject_pairs_dir.mkdir(exist_ok=True)
+        ids_dir = ids_with_n_subject_pairs_dir / 'ids'
+        ids_dir.mkdir(exist_ok=True)
+
+        # Loop to create 1000 random subject samples of the same size (with replacement) per bootstrap sample
+        n_bootstrap = 1000
+        for i_bootstrap in range(n_bootstrap):
+            # Create empty df to add bootstrap subjects to
+            dataset_bootstrap_train = pd.DataFrame(columns=['Participant_ID'])
+
+            # Loop over ages (27 in total)
+            for age in range(age_min, (age_max + 1)):
+
+                # Get dataset for specific age only
+                age_group = dataset.groupby('Age').get_group(age)
+
+                # Loop over genders (0: female, 1:male)
+                for gender in range(2):
+                    gender_group = age_group.groupby('Gender').get_group(gender)
+
+                    # Extract random subject of that gender and add to dataset_bootstrap_train
+                    random_sample_train = gender_group.sample(n=i_n_subject_pairs, replace=True)
+                    dataset_bootstrap_train = pd.concat([dataset_bootstrap_train, pd.DataFrame(random_sample_train['Participant_ID'])])
+
+            # Export dataset_bootstrap_train as csv
             ids_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}.csv'.format(i_bootstrap, i_n_subject_pairs)
-            dataset_bootstrap.to_csv(ids_dir / ids_filename, index=False)
+            dataset_bootstrap_train.to_csv(ids_dir / ids_filename, index=False)
+
+            ids_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}.csv'.format(i_bootstrap, i_n_subject_pairs)
+            dataset_bootstrap_train.to_csv(ids_dir / ids_filename, index=False)
+
 
 
 if __name__ == "__main__":
