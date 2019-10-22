f95e6bd488556864650fe56292c7e7e13305502f
Warvito <ianonimato@hotmail.com>
2019-10-22 11:40:26 +0100

    Create datasets for training and test

--- create_h5_bootstrap.py
+++ create_h5_bootstrap.py
@@ -30,13 +30,22 @@ def main():
         # Loop over the 1000 random subject samples per bootstrap
         n_bootstrap = 1000
         for i_bootstrap in range(n_bootstrap):
-            ids_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}.csv'.format(i_bootstrap, i_n_subject_pairs)
-            id_path = ids_with_n_subject_pairs_dir / 'ids' / ids_filename
+            training_ids_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}_train.csv'.format(i_bootstrap,
+                                                                                             i_n_subject_pairs)
+            training_id_path = ids_with_n_subject_pairs_dir / 'ids' / training_ids_filename
 
-            dataset_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}.h5'.format(i_bootstrap, i_n_subject_pairs)
+            dataset_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}_train.h5'.format(i_bootstrap, i_n_subject_pairs)
             dataset_path = dataset_dir / dataset_filename
 
-            create_dataset(demographic_path, id_path, freesurfer_path, dataset_path)
+            create_dataset(demographic_path, training_id_path, freesurfer_path, dataset_path)
+
+            test_ids_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}_train.csv'.format(i_bootstrap, i_n_subject_pairs)
+            test_id_path = ids_with_n_subject_pairs_dir / 'ids' / test_ids_filename
+
+            dataset_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}_test.h5'.format(i_bootstrap, i_n_subject_pairs)
+            dataset_path = dataset_dir / dataset_filename
+
+            create_dataset(demographic_path, test_id_path, freesurfer_path, dataset_path)
 
 
 if __name__ == "__main__":
