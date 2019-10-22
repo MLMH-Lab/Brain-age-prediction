c7053fbd20e8fa79d37d85fcf412c7c78e3a4777
Warvito <ianonimato@hotmail.com>
2019-10-22 11:58:35 +0100

    Add draft bootstrap

--- svm_bootstrap.py
+++ svm_bootstrap.py
@@ -11,11 +11,9 @@ import warnings
 from scipy import stats
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import KFold
 from sklearn.preprocessing import RobustScaler
 from sklearn.svm import LinearSVR
 from sklearn.metrics import mean_absolute_error, mean_squared_error
-from sklearn.model_selection import GridSearchCV
 
 from utils import COLUMNS_NAME
 
@@ -29,6 +27,16 @@ def main():
     experiment_name = 'biobank_scanner1'
     experiment_dir = PROJECT_ROOT / 'outputs' / experiment_name
     # ----------------------------------------------------------------------------------------
+    # Add independent dataset biobank scanner 2
+    independent_dataset_path = experiment_dir / 'biobank2.h5'
+    independent_dataset = pd.read_hdf(independent_dataset_path, key='table')
+
+    regions = independent_dataset[COLUMNS_NAME].values
+    tiv = independent_dataset.EstimatedTotalIntraCranialVol.values[:, np.newaxis]
+
+    x_independent = np.true_divide(regions, tiv)
+    y_independent = independent_dataset['Age'].values
+
 
     # Loop over the 50 bootstrap samples with up to 50 gender-balanced subject pairs per age group/year
     for i_n_subject_pairs in range(1, 51):
@@ -44,89 +52,80 @@ def main():
         n_bootstrap = 1000
         for i_bootstrap in range(n_bootstrap):
             print('Sample number within bootstrap: ', i_bootstrap)
-            dataset_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}.h5'.format(i_bootstrap, i_n_subject_pairs)
-            dataset_path = dataset_dir / dataset_filename
+            training_dataset_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}_train.h5'.format(i_bootstrap, i_n_subject_pairs)
+            test_dataset_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}_test.h5'.format(i_bootstrap, i_n_subject_pairs)
 
             # Load hdf5 dataset for that bootstrap sample
-            dataset = pd.read_hdf(dataset_path, key='table')
+            train_dataset_path = dataset_dir / training_dataset_filename
+            train_dataset = pd.read_hdf(train_dataset_path, key='table')
+
+            test_dataset_path = dataset_dir / test_dataset_filename
+            test_dataset = pd.read_hdf(test_dataset_path, key='table')
 
             # Initialise random seed
             np.random.seed(42)
             random.seed(42)
 
             # Normalise regional volumes by total intracranial volume (tiv)
-            regions = dataset[COLUMNS_NAME].values
+            regions = train_dataset[COLUMNS_NAME].values
 
-            tiv = dataset.EstimatedTotalIntraCranialVol.values[:, np.newaxis]
+            tiv = train_dataset.EstimatedTotalIntraCranialVol.values[:, np.newaxis]
 
-            regions_norm = np.true_divide(regions, tiv)
-            age = dataset['Age'].values
+            x_train = np.true_divide(regions, tiv)
+            y_train = train_dataset['Age'].values
 
-            # Create variable to hold CV variables
-            cv_r2_scores = []
-            cv_mae = []
-            cv_rmse = []
-            cv_age_error_corr = []
+            test_tiv = test_dataset.EstimatedTotalIntraCranialVol.values[:, np.newaxis]
+            test_regions = test_dataset[COLUMNS_NAME].values
 
-            n_repetitions = 10
-            n_folds = 10
-            n_nested_folds = 5
+            x_test = np.true_divide(test_regions, test_tiv)
+            y_test = test_dataset['Age'].values
 
-            for i_repetition in range(n_repetitions):
-                # Create 10-fold cross-validator
-                kf = KFold(n_splits=n_folds, shuffle=True, random_state=i_repetition)
-                for i_fold, (train_index, test_index) in enumerate(kf.split(regions_norm, age)):
-                    x_train, x_test = regions_norm[train_index], regions_norm[test_index]
-                    y_train, y_test = age[train_index], age[test_index]
+            # Scaling in range [-1, 1]
+            scaler = RobustScaler()
+            x_train = scaler.fit_transform(x_train)
+            x_test = scaler.transform(x_test)
+            x_independent_norm = scaler.transform(x_independent)
 
-                    # Scaling in range [-1, 1]
-                    scaler = RobustScaler()
-                    x_train = scaler.fit_transform(x_train)
-                    x_test = scaler.transform(x_test)
+            # Systematic search for best hyperparameters
+            # TODO: Change to Pedro's RVR
+            svm = LinearSVR(loss='epsilon_insensitive')
+            svm.fit(x_train, y_train)
+            predictions = svm.predict(x_test)
 
-                    # Systematic search for best hyperparameters
-                    svm = LinearSVR(loss='epsilon_insensitive')
+            absolute_error = mean_absolute_error(y_test, predictions)
+            root_squared_error = sqrt(mean_squared_error(y_test, predictions))
+            r2_score = svm.score(x_test, y_test)
+            age_error_corr, _ = stats.spearmanr(np.abs(y_test - predictions), y_test)
 
-                    search_space = {'C': [2 ** -7, 2 ** -5, 2 ** -3, 2 ** -1, 2 ** 0, 2 ** 1, 2 ** 3, 2 ** 5, 2 ** 7]}
+            print('Mean R2: {:0.3f}, MAE: {:0.3f}, RMSE: {:0.3f}, CORR: {:0.3f}'.format(r2_score,
+                                                                                        absolute_error,
+                                                                                        root_squared_error,
+                                                                                        age_error_corr))
 
-                    nested_kf = KFold(n_splits=n_nested_folds, shuffle=True, random_state=i_repetition)
-
-                    gridsearch = GridSearchCV(svm,
-                                              param_grid=search_space,
-                                              scoring='neg_mean_absolute_error',
-                                              refit=True, cv=nested_kf,
-                                              verbose=0, n_jobs=1)
-
-                    gridsearch.fit(x_train, y_train)
-
-                    best_svm = gridsearch.best_estimator_
-
-                    predictions = best_svm.predict(x_test)
-
-                    absolute_error = mean_absolute_error(y_test, predictions)
-                    root_squared_error = sqrt(mean_squared_error(y_test, predictions))
-                    r2_score = best_svm.score(x_test, y_test)
-                    age_error_corr, _ = stats.spearmanr(np.abs(y_test - predictions), y_test)
-
-                    cv_r2_scores.append(r2_score)
-                    cv_mae.append(absolute_error)
-                    cv_rmse.append(root_squared_error)
-                    cv_age_error_corr.append(age_error_corr)
-
-            # Variables for CV means across all repetitions
-            cv_r2_mean = np.mean(cv_r2_scores)
-            cv_mae_mean = np.mean(cv_mae)
-            cv_rmse_mean = np.mean(cv_rmse)
-            cv_age_error_corr_mean = np.mean(np.abs(cv_age_error_corr))
-            print('Mean R2: {:0.3f}, MAE: {:0.3f}, RMSE: {:0.3f}, CORR: {:0.3f}'.format(cv_r2_mean,
-                                                                                        cv_mae_mean,
-                                                                                        cv_rmse_mean,
-                                                                                        cv_age_error_corr_mean))
-
-            mean_scores = np.array([cv_r2_mean, cv_mae_mean, cv_rmse_mean, cv_age_error_corr_mean])
+            mean_scores = np.array([r2_score, absolute_error, root_squared_error, age_error_corr])
 
             # Save arrays with permutation coefs and scores as np files
             filepath_scores = scores_dir / ('boot_scores_{:04d}.npy'.format(i_bootstrap))
+            np.save(str(filepath_scores), mean_scores)
+
+
+            # Independent dataset evaluation
+            independent_predictions = svm.predict(x_independent_norm)
+
+            absolute_error = mean_absolute_error(y_independent, independent_predictions)
+            root_squared_error = sqrt(mean_squared_error(y_independent, independent_predictions))
+            r2_score = svm.score(x_independent_norm, y_independent)
+            age_error_corr, _ = stats.spearmanr(np.abs(y_independent - independent_predictions), y_independent)
+
+            print('Mean R2: {:0.3f}, MAE: {:0.3f}, RMSE: {:0.3f}, CORR: {:0.3f}'.format(r2_score,
+                                                                                        absolute_error,
+                                                                                        root_squared_error,
+                                                                                        age_error_corr))
+
+            mean_scores = np.array([r2_score, absolute_error, root_squared_error, age_error_corr])
+
+            # Save arrays with permutation coefs and scores as np files
+            filepath_scores = scores_dir / ('boot_scores_{:04d}_independent.npy'.format(i_bootstrap))
             np.save(str(filepath_scores), mean_scores)
 
 
