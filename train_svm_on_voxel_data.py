baddfc4006662972a74e2d7a85b2c4b642b93a42
Jessica Dafflon <jejedafflon@gmail.com>
2019-11-19 16:09:55 +0000

    WIP

--- train_svm_on_voxel_data.py
+++ train_svm_on_voxel_data.py
@@ -5,9 +5,6 @@ from math import sqrt
 from pathlib import Path
 import random
 import warnings
-import glob
-import re
-
 from scipy import stats
 import pandas as pd
 import numpy as np
@@ -17,7 +14,6 @@ from sklearn.svm import SVR
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.externals.joblib import dump
 from sklearn.model_selection import GridSearchCV
-import nibabel as nib
 from nilearn.masking import apply_mask
 
 PROJECT_ROOT = Path.cwd()
@@ -26,63 +22,36 @@ warnings.filterwarnings('ignore')
 
 def main():
     # ----------------------------------------------------------------------------------------
-    experiment_name = 'biobank_scanner1_voxel'
+    experiment_name = 'biobank_scanner1'
 
     # TODO: Change the path. Here you should load the voxel data?
-    dataset_path = Path('/Volumes/Elements/BIOBANK/SCANNER01')
-    # Load the demographics file
+    # dataset_path = Path('/Volumes/Elements/BIOBANK/SCANNER01')
+    dataset_path = Path('/media/jed15/ELEMENTS/BIOBANK/SCANNER01')
+    kernel_path = PROJECT_ROOT / 'outputs' / 'kernels' / 'kernel.csv'
+
+    # Load demographics
     demographics = pd.read_csv((dataset_path / 'participants.tsv'), sep='\t')
     demographics.set_index('Participant_ID', inplace=True)
 
-    # Get list of subjects for which we have data
-    subjects_path = glob.glob(str(dataset_path / 'sub-*Warped.nii.gz'))
-    subjects_path.sort()
+    # Load the Gram matrix
+    kernel = pd.read_csv(kernel_path, header=0, index_col=0)
 
-    # Get only the subject's ID from their path
-    subjects_id = [re.findall('sub-\d+', subject)[0] for subject in
-                   subjects_path]
-    # TODO: Get a list of 100 subjects, for testing purpuses
-    import pdb
-    pdb.set_trace()
-    subjects_id = subjects_id[:100]
+    # Get demographics for subject's ID in the kernel
+    demographics = demographics[demographics.index.isin(kernel.index)]
 
-    # Get demographics only for the subjects we have information for
-    demographics = demographics[demographics.index.isin(subjects_id)]
-
-    # Load the mask image
-    brain_mask = PROJECT_ROOT / 'imaging_preprocessing_ANTs' / \
-                 'mni_icbm152_t1_tal_nlin_sym_09c_mask.nii'
-    mask_img = nib.load(str(brain_mask))
-
-    data = {}
-    # Load images for the subjects
-    for idx, subject_path in enumerate(subjects_path):
-        print('Subject: %s, Data Path: %s' % (subjects_id[idx], subject_path))
-        img = nib.load(subject_path)
-        # Extract only the brain voxels. This will create a 1D array.
-        masked_data = apply_mask(img, mask_img)
-
-        # Save information for current subject
-        data[subjects_id[idx]] = np.float16(masked_data)
-
-    # Compute Gram-matrix
+    # Compute SVM
     # ----------------------------------------------------------------------------------------
-    print('STOP!')
-    pd.set_trace()
     experiment_dir = PROJECT_ROOT / 'outputs' / experiment_name
-    svm_dir = experiment_dir / 'SVM'
-    svm_dir.mkdir(exist_ok=True)
+    svm_dir = experiment_dir / 'voxel_SVM'
+    svm_dir.mkdir(exist_ok=True, parents=True)
     cv_dir = svm_dir / 'cv'
-    cv_dir.mkdir(exist_ok=True)
+    cv_dir.mkdir(exist_ok=True, parents=True)
 
     # Initialise random seed
     np.random.seed(42)
     random.seed(42)
 
-    # Load hdf5 file
-    dataset = pd.read_hdf(dataset_path, key='table')
-
-    age = dataset['Age'].values
+    age = demographics['Age'].values
 
     # Cross validation variables
     cv_r2_scores = []
@@ -90,9 +59,8 @@ def main():
     cv_rmse = []
     cv_age_error_corr = []
 
-    # Create dataframe to hold actual and predicted ages
-    age_predictions = pd.DataFrame(dataset[['Participant_ID', 'Age']])
-    age_predictions = age_predictions.set_index('Participant_ID')
+    # Create Dataframe to hold actual and predicted ages
+    age_predictions = pd.DataFrame(demographics['Age'])
 
     n_repetitions = 10
     n_folds = 10
@@ -105,19 +73,20 @@ def main():
 
         # Create 10-fold cross-validation scheme stratified by age
         skf = StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=i_repetition)
-        for i_fold, (train_index, test_index) in enumerate(skf.split(regions_norm, age)):
+        for i_fold, (train_index, test_index) in enumerate(skf.split(kernel, age)):
             print('Running repetition {:02d}, fold {:02d}'.format(i_repetition, i_fold))
 
-            x_train, x_test = regions_norm[train_index], regions_norm[test_index]
+            x_train = kernel.iloc[train_index, train_index]
+            x_test = kernel.iloc[test_index, test_index]
             y_train, y_test = age[train_index], age[test_index]
 
             # Scaling using inter-quartile
-            scaler = RobustScaler()
-            x_train = scaler.fit_transform(x_train)
-            x_test = scaler.transform(x_test)
+            # scaler = RobustScaler()
+            # x_train = scaler.fit_transform(x_train)
+            # x_test = scaler.transform(x_test)
 
             # Systematic search for best hyperparameters
-            svm = SVR(loss='epsilon_insensitive')
+            svm = SVR(kernel='precomputed', loss='epsilon_insensitive')
 
             search_space = {'C': [2 ** -7, 2 ** -5, 2 ** -3, 2 ** -1, 2 ** 0,
                                   2 ** 1, 2 ** 3, 2 ** 5, 2 ** 7]}
@@ -155,12 +124,13 @@ def main():
             model_filename = '{:02d}_{:02d}_regressor.joblib'.format(i_repetition, i_fold)
             params_filename = '{:02d}_{:02d}_params.joblib'.format(i_repetition, i_fold)
 
-            dump(scaler, cv_dir / scaler_filename)
+            # dump(scaler, cv_dir / scaler_filename)
             dump(params_results, cv_dir / params_filename)
             dump(best_svm, cv_dir / model_filename)
 
             # Save model scores r2, MAE, RMSE
-            scores_array = np.array([r2_score, absolute_error, root_squared_error, age_error_corr])
+            scores_array = np.array([r2_score, absolute_error, root_squared_error,
+                                     age_error_corr])
 
             scores_filename = '{:02d}_{:02d}_scores.npy'.format(i_repetition, i_fold)
 
@@ -173,6 +143,8 @@ def main():
             # Print results of the CV fold
             print('Repetition {:02d}, Fold {:02d}, R2: {:0.3f}, MAE: {:0.3f}, RMSE: {:0.3f}, CORR: {:0.3f}'
                   .format(i_repetition, i_fold, r2_score, absolute_error, root_squared_error, age_error_corr))
+            import pdb
+            pdb.set_trace()
 
     # Save predictions
     age_predictions.to_csv(svm_dir / 'age_predictions.csv')
