7780e1e29f5629cdfe00b81abbbf60c60a1614d6
Warvito <ianonimato@hotmail.com>
2020-01-06 14:30:42 +0000

    Change how the coeficients are stored.

--- visualise_importance_on_voxel_data.py
+++ visualise_importance_on_voxel_data.py
@@ -24,38 +24,25 @@ def main():
     cv_dir = svm_dir / 'cv'
     print(feat_imp_dir)
     # --------------------------------------------------------------------------
-
     assessed_model_coefs = []
-    n_repetitions = 1
+    n_repetitions = 10
+    n_folds = 10
     for i_repetition in range(n_repetitions):
-        weights_filename = '{:02d}_weights.npy'.format(i_repetition)
-        coefs = np.load(cv_dir / weights_filename)
-        assessed_model_coefs.append(coefs)
+        for i_fold in range(n_folds):
+            importance_filename = '{:02d}_{:02d}_importance.nii.gz'.format(i_repetition, i_fold)
+            importance_map = nib.load(str(cv_dir / importance_filename))
+            data_map = importance_map.get_fdata()
+            assessed_model_coefs.append(np.abs(data_map) / np.sum(np.abs(data_map), axis=(0, 1, 2)))
 
     assessed_model_coefs = np.array(assessed_model_coefs)
-    assessed_model_coefs = assessed_model_coefs.reshape(-1, assessed_model_coefs.shape[-1])
-
-    assessed_mean_relative_coefs = np.divide(np.abs(assessed_model_coefs),
-                                             np.sum(np.abs(assessed_model_coefs), axis=1)[:, np.newaxis])
-    final_coefs = np.mean(assessed_mean_relative_coefs, axis=0)
-    # --------------------------------------------------------------------------
-
-    # Load brain mask and find index for the voxels inside the mask
-    brain_mask = PROJECT_ROOT / 'imaging_preprocessing_ANTs' / 'mni_icbm152_t1_tal_nlin_sym_09c_mask.nii'
-    mask_img = nib.load(str(brain_mask))
-    mask_data = mask_img.get_fdata()
-    coords = np.argwhere(mask_data > 0)
-
-    importance_map = np.zeros_like(mask_data)
-    for xyz, importance in zip(coords, final_coefs):
-        importance_map[tuple(xyz)] = importance
+    final_coefs = np.mean(assessed_model_coefs, axis=0)
 
     # --------------------------------------------------------------------------
-    importance_map_nifti = nib.Nifti1Image(importance_map, mask_img.affine)
+    importance_map_nifti = nib.Nifti1Image(final_coefs, importance_map.affine)
 
     # Select central slice for the X and Y coordinate
-    x = importance_map_nifti.shape[0]/2
-    y = importance_map_nifti.shape[1]/2
+    x = importance_map_nifti.shape[0] / 2
+    y = importance_map_nifti.shape[1] / 2
 
     # Define min and max slice to analyse
     z_min = 28
@@ -63,8 +50,19 @@ def main():
 
     # Load anatomical template image
     template = (PROJECT_ROOT / 'imaging_preprocessing_ANTs' /
-    'mni_icbm152_t1_tal_nlin_sym_09c_brain.nii.gz')
+                'mni_icbm152_t1_tal_nlin_sym_09c.nii')
     template = nib.load(str(template))
+
+    brain_mask = PROJECT_ROOT / 'imaging_preprocessing_ANTs' / 'mni_icbm152_t1_tal_nlin_sym_09c_mask.nii'
+    mask_img = nib.load(str(brain_mask))
+    bg_img = template.get_fdata() * mask_img.get_fdata()
+    template = nib.Nifti1Image(bg_img, template.affine)
+    # TODO: Try to make it work with data from the http://nist.mni.mcgill.ca/?p=904 or
+    #  consider add to the repository the mni_icbm152_t1_tal_nlin_sym_09c_brain.nii.gz
+
+    # template = (PROJECT_ROOT / 'imaging_preprocessing_ANTs' /
+    # 'mni_icbm152_t1_tal_nlin_sym_09c_brain.nii.gz')
+    # template = nib.load(str(template))
 
     # Save multiple slices
     for i in range(z_min, z_max, 1):
@@ -75,10 +73,10 @@ def main():
                                bg_img=template,
                                cut_coords=coordinates,
                                output_file=feat_imp_dir /
-                               'feature_importance_{}.png'.format(i),
+                                           'feature_importance_{}.png'.format(i),
                                draw_cross=False,
                                cmap='Reds',
-                               threshold=thr_mean + 2 * thr_std,
+                               threshold=thr_mean + 2 * thr_std, #TODO: define these values
                                black_bg=False,
                                )
     # Make gif
@@ -89,6 +87,7 @@ def main():
         for fname in png_list:
             image = imageio.imread(fname)
             writer.append_data(image)
+
 
 if __name__ == "__main__":
     main()
