86161031e86d63bfa81075a2b0885ac1e4080569
Warvito <ianonimato@hotmail.com>
2020-01-08 15:50:39 +0000

    WIP: saving scores for testing

--- test_svm_on_freesurfer_data.py
+++ test_svm_on_freesurfer_data.py
@@ -3,13 +3,16 @@ on previously unseen data from Biobank Scanner2 to predict brain age.
 
 The script loops over the 100 SVM models created in train_svm_on_freesurfer_data.py, loads their regressors,
 applies them to the Scanner2 data and saves all predictions per subjects in a csv file"""
-
+from math import sqrt
 from pathlib import Path
 from sklearn.externals.joblib import load
 
+from scipy import stats
 import numpy as np
 import pandas as pd
 import random
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
 
 from utils import COLUMNS_NAME
 
@@ -31,18 +34,19 @@ def main():
     random.seed(42)
 
     # Load hdf5 file with testing data
-    testing_dataset = pd.read_hdf(testing_dataset_path, key='table')
+    dataset = pd.read_hdf(testing_dataset_path, key='table')
 
     # Normalise regional volumes in testing dataset by total intracranial volume (tiv)
-    regions = testing_dataset[COLUMNS_NAME].values
+    regions = dataset[COLUMNS_NAME].values
 
-    tiv = testing_dataset.EstimatedTotalIntraCranialVol.values[:, np.newaxis]
+    tiv = dataset.EstimatedTotalIntraCranialVol.values[:, np.newaxis]
 
     regions_norm = np.true_divide(regions, tiv)
+    age = dataset['Age'].values
 
-    # Create dataframe to hold actual and predicted ages in testing dataset
-    testset_age_predictions = pd.DataFrame(testing_dataset[['Participant_ID', 'Age']])
-    testset_age_predictions = testset_age_predictions.set_index('Participant_ID')
+    # Create dataframe to hold actual and predicted ages
+    age_predictions = pd.DataFrame(dataset[['Participant_ID', 'Age']])
+    age_predictions = age_predictions.set_index('Participant_ID')
 
     # Create list of SVM model prefixes
     n_repetitions = 10
@@ -58,17 +62,27 @@ def main():
             scaler = load(svm_cv_dir / scaler_filename)
 
             # Use RobustScaler to transform testing data
-            regions_norm_test = scaler.transform(regions_norm)
+            x_test = scaler.transform(regions_norm)
 
             # Apply regressors to scaled data
-            test_predictions = regressor.predict(regions_norm_test)
+            predictions = regressor.predict(x_test)
+
+            absolute_error = mean_absolute_error(age, predictions)
+            root_squared_error = sqrt(mean_squared_error(age, predictions))
+            r2_score = regressor.score(x_test, age)
+            age_error_corr, _ = stats.spearmanr(np.abs(age - predictions), age)
 
             # Save prediction per model in df
-            testset_age_predictions[('{:02d}_{:02d}'.format(i_repetition, i_fold))] = test_predictions
+            age_predictions[('{:02d}_{:02d}'.format(i_repetition, i_fold))] = predictions
+
+            # Save model scores r2, MAE, RMSE
+            scores_array = np.array([r2_score, absolute_error, root_squared_error, age_error_corr])
+            scores_filename = '{:02d}_{:02d}_scores.npy'.format(i_repetition, i_fold)
+            np.save(cv_dir / scores_filename, scores_array)
 
     # Export df as csv
     testset_age_predictions_filename = PROJECT_ROOT / 'outputs' / testing_experiment_name / 'svm_testset_predictions.csv'
-    testset_age_predictions.to_csv(testset_age_predictions_filename)
+    age_predictions.to_csv(testset_age_predictions_filename)
 
 if __name__ == "__main__":
     main()
