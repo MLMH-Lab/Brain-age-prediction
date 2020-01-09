71b31b8488b0fdb894cbcd6406a766ddbefc589b
Warvito <ianonimato@hotmail.com>
2020-01-09 16:43:37 +0000

    fix conflicts

--- clean_biobank2_data.py
+++ clean_biobank2_data.py
@@ -1,6 +1,4 @@
-"""TEMPORARY SCRIPT to clean Biobank scanner 2 data.
-This script may be incorporated into clean_biobank1_data script eventually."""
-
+"""Clean UK Biobank scanner 2 data."""
 from pathlib import Path
 
 import pandas as pd
@@ -13,14 +11,16 @@ PROJECT_ROOT = Path.cwd()
 def main():
     """Clean UK Biobank scanner 2 data."""
     # ----------------------------------------------------------------------------------------
-    experiment_name = 'biobank_scanner2'
+    experiment_name = 'biobank_scanner1'
 
-    #TODO: update script to only use tsv file for demographic data
     demographic_path = PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner2' / 'ukb22321.csv'
     participants_path = PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner2' / 'participants.tsv'
     id_path = PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner2' / 'freesurferData.csv'
+    qoala_path = PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner2' / 'Qoala_T_predictions_SCANNER02.csv'
+    mriqc_path = PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner2' / 'group_T1w.tsv'
+    mriqc2_path = PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner2' / 'BIOBANKS2-unseen_pred.csv'
 
-    output_ids_filename = 'cleaned_ids_scanner2_noqc.csv'
+    output_ids_filename = 'biobank_scanner2_cleaned_ids.csv'
     # ----------------------------------------------------------------------------------------
     # Create experiment's output directory
     experiment_dir = PROJECT_ROOT / 'outputs' / experiment_name
@@ -28,8 +28,7 @@ def main():
 
     dataset = load_demographic_data(demographic_path, id_path)
 
-    participants_df = pd.read_csv(participants_path, sep='\t', usecols=['participant_id', 'Diagn'])
-    participants_df.columns = ['Participant_ID', 'Diagn']
+    participants_df = pd.read_csv(participants_path, sep='\t', usecols=['Participant_ID', 'Diagn'])
     participants_df['ID'] = participants_df['Participant_ID'].str.split('-').str[1]
     participants_df['ID'] = pd.to_numeric(participants_df['ID'])
 
@@ -43,14 +42,27 @@ def main():
 
     # Exclude patients
     dataset = dataset[dataset['Diagn'] == 1]
+    # ----------------------------------------------------------------------------------------
+    # Clean data based on quality control
+    # qoala_df = pd.read_csv(qoala_path, usecols=['image_id', 'prob_qoala'])
+    # qoala_df = qoala_df.rename(columns={"image_id": "Image_ID"})
+    # dataset = pd.merge(dataset, qoala_df, on='Image_ID')
+    #
+    # mriqc_df = pd.read_csv(mriqc_path, sep='\t', usecols=['bids_name'])
+    # mriqc2_df = pd.read_csv(mriqc2_path, usecols=['subject_id', 'prob_y'])
+    #
+    # mriqc_data_df = pd.concat((mriqc_df, mriqc2_df), axis=1)
+    # mriqc_data_df['Image_ID'] = mriqc_data_df['bids_name'] + '/'
+    # mriqc_data_df = mriqc_data_df.rename(columns={"prob_y": "mriqc_prob"})
+    #
+    # dataset_df = pd.merge(dataset, mriqc_data_df, on='Image_ID')
+    #
+    # mriqc_threshold = 0.6
+    # qoala_threshold = 0.4
+    # selected_subjects = dataset_df[(dataset_df['mriqc_prob'] < mriqc_threshold) & (dataset_df['prob_qoala'] < qoala_threshold)]
 
     output_ids_df = pd.DataFrame(dataset['Participant_ID'])
     output_ids_df.to_csv(experiment_dir / output_ids_filename, index=False)
-
-    #Create hdf5 file
-    dataset_path = PROJECT_ROOT / 'outputs' / experiment_name / 'freesurferData.h5'
-    dataset.to_hdf(dataset_path, key='table', mode='w')
-
 
 
 if __name__ == "__main__":
