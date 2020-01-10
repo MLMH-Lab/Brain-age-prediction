b90164506bfc10a379c8c513da2a267e030d3956
Warvito <ianonimato@hotmail.com>
2020-01-10 11:04:56 +0000

    Fix nomenclature

--- preprocessing_homogenize_gender.py
+++ preprocessing_homogenize_gender.py
@@ -21,16 +21,21 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-E', '--experiment_name',
                     dest='experiment_name',
                     help='Name of the experiment.')
+parser.add_argument('-S', '--scanner_name',
+                    dest='scanner_name',
+                    help='Name of the scanner.')
+parser.add_argument('-I', '--input_ids_file',
+                    dest='input_ids_file',
+                    help='Filename indicating the ids to be used.')
 args = parser.parse_args()
 
 
 def check_balance_across_groups(crosstab_df):
-    """"""
+    """Verify if which age pair have gender imbalance."""
     combinations = list(itertools.combinations(crosstab_df.columns, 2))
     significance_level = 0.05 / len(combinations)
 
     for group1, group2 in combinations:
-        # print('{} vs {}'.format(group1, group2))
         contingency_table = crosstab_df[[group1, group2]]
         _, p_value, _, _ = stats.chi2_contingency(contingency_table, correction=False)
 
@@ -49,54 +54,54 @@ def get_problematic_group(crosstab_df):
 
     conditions_proportions = crosstab_df.apply(lambda r: r / r.sum(), axis=0)
     median_proportion = np.median(conditions_proportions.values[0, :])
-    problematic = conditions_proportions[problematic_groups].values[0, :]
+    problematic_proportion = conditions_proportions[problematic_groups].values[0, :]
 
-    problematic_group = problematic_groups[np.argmax(np.abs(problematic - median_proportion))]
+    problematic_group = problematic_groups[np.argmax(np.abs(problematic_proportion - median_proportion))]
 
     return problematic_group
 
 
-def get_balanced_dataset(dataset):
-    """"""
+def get_balanced_dataset(dataset_df):
+    """Script to perform gender balancing across the subjects' age range."""
 
     while True:
-        crosstab_df = pd.crosstab(dataset['Gender'], dataset['Age'])
+        crosstab_df = pd.crosstab(dataset_df['Gender'], dataset_df['Age'])
 
         problematic_group = get_problematic_group(crosstab_df)
 
         if problematic_group is None:
             break
 
-        condition_unbalanced = crosstab_df[problematic_group].idxmax()
+        condition_imbalanced = crosstab_df[problematic_group].idxmax()
 
-        problematic_group_mask = (dataset['Age'] == problematic_group) & \
-                                 (dataset['Gender'] == condition_unbalanced)
+        problematic_group_mask = (dataset_df['Age'] == problematic_group) & \
+                                 (dataset_df['Gender'] == condition_imbalanced)
 
-        list_to_drop = list(dataset[problematic_group_mask].sample(1).index)
-        print('Dropping {:}'.format(dataset['Image_ID'].iloc[list_to_drop].values[0]))
-        dataset = dataset.drop(list_to_drop, axis=0)
+        list_to_drop = list(dataset_df[problematic_group_mask].sample(1).index)
+        print('Dropping {:}'.format(dataset_df['Image_ID'].iloc[list_to_drop].values[0]))
+        dataset_df = dataset_df.drop(list_to_drop, axis=0)
 
-    return dataset
+    return dataset_df
 
 
-def main(experiment_name, scanner_name):
+def main(experiment_name, scanner_name, input_ids_file):
     """Perform the exploratory data analysis."""
     participants_path = PROJECT_ROOT / 'data' / 'BIOBANK' / scanner_name / 'participants.tsv'
-    # ids_path = PROJECT_ROOT / 'outputs' / experiment_name / 'cleaned_ids.csv' #TODO: Use this one
-    ids_path = PROJECT_ROOT / 'outputs' / experiment_name / 'cleaned_ids_noqc.csv'
+    ids_path = PROJECT_ROOT / 'outputs' / experiment_name / 'cleaned_ids.csv'
 
     experiment_dir = PROJECT_ROOT / 'outputs' / experiment_name
 
     # Define random seed for sampling methods
     np.random.seed(42)
 
-    dataset = load_demographic_data(participants_path, ids_path)
+    dataset_df = load_demographic_data(participants_path, ids_path)
 
-    dataset_balanced = get_balanced_dataset(dataset)
+    dataset_balanced = get_balanced_dataset(dataset_df)
 
-    homogeneous_ids = dataset_balanced[['participant_id']]
-    homogeneous_ids.to_csv(experiment_dir / 'homogenized_ids.csv', index=False)
+    homogeneous_ids_df = dataset_balanced[['Image_ID']]
+    homogeneous_ids_df.to_csv(experiment_dir / 'homogenized_ids.csv', index=False)
 
 
 if __name__ == "__main__":
-    main(args.experiment_name)
+    main(args.experiment_name, args.scanner_name,
+         args.input_ids_file)
