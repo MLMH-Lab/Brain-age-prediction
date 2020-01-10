9968e0f12ee42736a9811b2a66874460d958b1d8
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2020-01-10 01:34:54 +0000

    Change ids column name from Participant_ID to Image_ID

--- sample_size_create_ids.py
+++ sample_size_create_ids.py
@@ -20,12 +20,12 @@ def main():
     experiment_name = 'biobank_scanner1'
 
     demographic_path = PROJECT_ROOT / 'data' / 'BIOBANK' / 'Scanner1' / 'participants.tsv'
-    id_path = PROJECT_ROOT / 'outputs' / experiment_name / 'dataset_homogeneous.csv'
+    id_path = PROJECT_ROOT / 'outputs' / experiment_name / 'homogenized_ids.csv'
     # ----------------------------------------------------------------------------------------
     # Create experiment's output directory
     experiment_dir = PROJECT_ROOT / 'outputs' / experiment_name
-    bootstrap_dir = experiment_dir / 'bootstrap_analysis'
-    bootstrap_dir.mkdir(exist_ok=True)
+    sample_size_dir = experiment_dir / 'sample_size'
+    sample_size_dir.mkdir(exist_ok=True)
 
     # Set random seed for random sampling of subjects
     np.random.seed(42)
@@ -42,7 +42,7 @@ def main():
     # Create a out-of-bag set (~test set)
     for i_n_subject_pairs in range(1, n_max_pair + 1):
         print(i_n_subject_pairs)
-        ids_with_n_subject_pairs_dir = bootstrap_dir / '{:02d}'.format(i_n_subject_pairs)
+        ids_with_n_subject_pairs_dir = sample_size_dir / '{:02d}'.format(i_n_subject_pairs)
         ids_with_n_subject_pairs_dir.mkdir(exist_ok=True)
         ids_dir = ids_with_n_subject_pairs_dir / 'ids'
         ids_dir.mkdir(exist_ok=True)
@@ -51,8 +51,8 @@ def main():
         n_bootstrap = 1000
         for i_bootstrap in range(n_bootstrap):
             # Create empty df to add bootstrap subjects to
-            dataset_bootstrap_train = pd.DataFrame(columns=['Participant_ID'])
-            dataset_bootstrap_test = pd.DataFrame(columns=['Participant_ID'])
+            dataset_bootstrap_train = pd.DataFrame(columns=['Image_ID'])
+            dataset_bootstrap_test = pd.DataFrame(columns=['Image_ID'])
 
             # Loop over ages (27 in total)
             for age in range(age_min, (age_max + 1)):
@@ -66,20 +66,19 @@ def main():
 
                     # Extract random subject of that gender and add to dataset_bootstrap_train
                     random_sample_train = gender_group.sample(n=i_n_subject_pairs, replace=True)
-                    dataset_bootstrap_train = pd.concat(
-                        [dataset_bootstrap_train, pd.DataFrame(random_sample_train['Participant_ID'])])
+                    dataset_bootstrap_train = pd.concat([dataset_bootstrap_train, random_sample_train[['Image_ID']]])
 
                     # Sample test set with always the same size
-                    not_sampled = ~gender_group['Participant_ID'].isin(random_sample_train['Participant_ID'])
+                    not_sampled = ~gender_group['Image_ID'].isin(random_sample_train['Image_ID'])
                     random_sample_test = gender_group[not_sampled].sample(n=n_max_pair, replace=False)
                     dataset_bootstrap_test = pd.concat(
-                        [dataset_bootstrap_test, pd.DataFrame(random_sample_test['Participant_ID'])])
+                        [dataset_bootstrap_test, pd.DataFrame(random_sample_test['Image_ID'])])
 
             # Export dataset_bootstrap_train as csv
-            ids_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}_train.csv'.format(i_bootstrap, i_n_subject_pairs)
+            ids_filename = 'sample_size_{:04d}_n_{:02d}_train.csv'.format(i_bootstrap, i_n_subject_pairs)
             dataset_bootstrap_train.to_csv(ids_dir / ids_filename, index=False)
 
-            ids_filename = 'homogeneous_bootstrap_{:04d}_n_{:02d}_test.csv'.format(i_bootstrap, i_n_subject_pairs)
+            ids_filename = 'sample_size_{:04d}_n_{:02d}_test.csv'.format(i_bootstrap, i_n_subject_pairs)
             dataset_bootstrap_test.to_csv(ids_dir / ids_filename, index=False)
 
 
