1ed8c007feedd5ee9ad7280d6421dc9f62329b46
Walter <walhugolp@gmail.com>
2020-03-13 15:27:45 +0000

    PC analysis

--- command_list.sh
+++ command_list.sh
@@ -31,7 +31,8 @@ export PYTHONPATH=$PYTHONPATH:./src
 
 # Create pca models
 ./src/preprocessing/create_pca_models.py -P "/media/kcl_1/SSD2/BIOBANK" -E "biobank_scanner1" -S "BIOBANK-SCANNER01"
-./calculating_principal_components.py -P "/media/kcl_1/SSD2/BIOBANK" -E "biobank_scanner1" -S "SCANNER01"
+./src/preprocessing/compute_principal_components.py -P "/media/kcl_1/SSD2/BIOBANK" -E "biobank_scanner1"
+./src/preprocessing/compute_principal_components.py -P "/media/kcl_1/HDD/DATASETS/BIOBANK/BIOBANK" -E "biobank_scanner2" -I "cleaned_ids.csv" -S "_general"
 
 # ----------------------------- Regressors comparison ------------------------------------
 ./src/comparison/comparison_fs_data_train_svm.py -E "biobank_scanner1" -S "BIOBANK-SCANNER01"
@@ -41,9 +42,11 @@ export PYTHONPATH=$PYTHONPATH:./src
 ./src/comparison/comparison_voxel_data_train_svm.py -E "biobank_scanner1" -S "BIOBANK-SCANNER01"
 ./src/comparison/comparison_voxel_data_train_rvm.py -E "biobank_scanner1" -S "BIOBANK-SCANNER01"
 
-./comparison_pca_data_train_rvm.py -E "biobank_scanner1" -S "SCANNER01"
+./src/comparison/comparison_pca_data_train_rvm.py -E "biobank_scanner1" -S "BIOBANK-SCANNER01"
+./src/comparison/comparison_pca_data_train_svm.py -E "biobank_scanner1" -S "BIOBANK-SCANNER01"
+./src/comparison/comparison_pca_data_train_gp.py -E "biobank_scanner1" -S "BIOBANK-SCANNER01"
 
-./src/comparison/comparison_statistical_analsysis.py -E "biobank_scanner1" -S "_all" -M "SVM" "RVM" "GPR" "voxel_SVM" "voxel_RVM"
+./src/comparison/comparison_statistical_analsysis.py -E "biobank_scanner1" -S "_all" -M "SVM" "RVM" "GPR" "voxel_SVM" "voxel_RVM" "pca_RVM" "pca_SVM" "pca_GPR"
 
 ./src/comparison/comparison_voxel_data_svm_primal_weights.py -E "biobank_scanner1" -P "/media/kcl_1/SSD2/BIOBANK"
 
@@ -59,9 +62,11 @@ export PYTHONPATH=$PYTHONPATH:./src
 ./src/generalisation/generalisation_test_voxel_data_svm.py -T "biobank_scanner1" -G "biobank_scanner2" -S "BIOBANK-SCANNER02" -M "voxel_SVM" -P "/media/kcl_1/HDD/DATASETS/BIOBANK/BIOBANK"
 ./src/generalisation/generalisation_test_voxel_data_rvm.py -T "biobank_scanner1" -G "biobank_scanner2" -S "BIOBANK-SCANNER02" -M "voxel_RVM" -P "/media/kcl_1/HDD/DATASETS/BIOBANK/BIOBANK"
 
-./generalisation_test_fs_data.py -T "biobank_scanner1" -G "biobank_scanner2" -S "SCANNER02" -M "pca_RVM" -I "cleaned_ids.csv"
+./src/generalisation/generalisation_test_pca_data.py -T "biobank_scanner1" -G "biobank_scanner2" -S "BIOBANK-SCANNER02" -M "pca_RVM" -I "cleaned_ids.csv"
+./src/generalisation/generalisation_test_pca_data.py -T "biobank_scanner1" -G "biobank_scanner2" -S "BIOBANK-SCANNER02" -M "pca_SVM" -I "cleaned_ids.csv"
+./src/generalisation/generalisation_test_pca_data.py -T "biobank_scanner1" -G "biobank_scanner2" -S "BIOBANK-SCANNER02" -M "pca_GPR" -I "cleaned_ids.csv"
 
-./src/comparison/comparison_statistical_analsysis.py -E "biobank_scanner2" -S "_generalization" -M "SVM" "RVM" "GPR" "voxel_SVM" "voxel_RVM"
+./src/comparison/comparison_statistical_analsysis.py -E "biobank_scanner2" -S "_generalization" -M "SVM" "RVM" "GPR" "voxel_SVM" "voxel_RVM" "pca_RVM" "pca_SVM" "pca_GPR"
 
 # ----------------------------- Sample size analysis ------------------------------------
 ./src/sample_size/sample_size_create_ids.py -E "biobank_scanner1" -S "BIOBANK-SCANNER01"
@@ -78,10 +83,6 @@ export PYTHONPATH=$PYTHONPATH:./src
 ./src/sample_size/sample_size_create_figures.py -E "biobank_scanner1" -M "GPR"
 ./src/sample_size/sample_size_create_figures.py -E "biobank_scanner1" -M "voxel_SVM"
 ./src/sample_size/sample_size_create_figures.py -E "biobank_scanner1" -M "voxel_RVM"
-
-# ----------------------------- Permutation ------------------------------------
-./permutation_lauch_subprocesses.py -E "biobank_scanner1" -S "BIOBANK-SCANNER01"
-./permutation_significance_test.py
 
 # ----------------------------- Covariates analysis ------------------------------------
 ./src/covariates/covariates_ensemble_output.py -E "biobank_scanner1" -M "SVM"
