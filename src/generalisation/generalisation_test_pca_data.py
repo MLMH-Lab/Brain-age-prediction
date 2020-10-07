Corrected calculation of age-brainage correlation in generalisation scripts

src/generalisation/generalisation_test_pca_data.py
@@ -110,7 +110,7 @@ def main(training_experiment_name, test_experiment_name, scanner_name, model_nam
             absolute_error = mean_absolute_error(age, predictions)
             root_squared_error = sqrt(mean_squared_error(age, predictions))
             r2 = r2_score(age, predictions)
-            age_error_corr, _ = stats.spearmanr(np.abs(age - predictions), age)
+            age_error_corr, _ = stats.spearmanr((predictions - age), age)
 
             # Save prediction per model in df
             age_predictions[f'Prediction {i_repetition:02d}_{i_fold:02d}'] = predictions

src/generalisation/generalisation_test_voxel_data_rvm.py
@@ -152,7 +152,7 @@ def main(training_experiment_name,
             mae = mean_absolute_error(age, predictions)
             rmse = sqrt(mean_squared_error(age, predictions))
             r2 = r2_score(age, predictions)
-            age_error_corr, _ = stats.spearmanr(np.abs(age - predictions), age)
+            age_error_corr, _ = stats.spearmanr((predictions - age), age)
 
             # Save model scores
             scores_array = np.array([r2, mae, rmse, age_error_corr])