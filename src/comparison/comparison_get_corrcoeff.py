f7feec139812da7367a66dcafbebd941aabf5009
leabaecker <k1802521@kcl.ac.uk>
2020-10-19 16:08:37 +0100

    updated and corrected comparison_get_corrcoeff.py for the calculation of r val and age bias

--- src/comparison/comparison_get_corrcoeff.py
+++ src/comparison/comparison_get_corrcoeff.py
@@ -1,6 +1,9 @@
-"""Script to calculate Pearson's r correlation coefficient
+"""Script to calculate Pearson's r correlation coefficient and
+age-BrainAGE correlation (also called 'age bias')
 for all model predictions per subject and chronological age
-in the CV iterations"""
+in the CV iterations
+
+TODO: possibly delete this script before publication, as it's covered in the training scripts"""
 
 import pandas as pd
 import numpy as np
@@ -18,18 +21,13 @@ def main():
                 'voxel_SVM', 'voxel_RVM',
                 'pca_SVM', 'pca_RVM', 'pca_GPR']
 
-    # TODO: check whether age_predictions_all is needed
-    # Create df with subject IDs and chronological age
-    # Based on an age_predictions csv file from model training to have the
-    # same order of subjects
-    example_file = pd.read_csv(experiment_dir / 'SVM' / 'age_predictions.csv')
-    age_predictions_all = pd.DataFrame(example_file.loc[:, 'image_id':'Age'])
+    # -------------------------------------
+    # Get Pearson's r correlation values for all models
 
     # Create df to hold r values for all models
     r_val_df = pd.DataFrame()
 
-    # Loop over all models, calculate mean predictions across repetitions,
-    # calculate r
+    # Loop over all models, calculate r
     for model_name in model_ls:
         model_dir = experiment_dir / model_name
         file_name = model_dir / 'age_predictions.csv'
@@ -60,27 +58,62 @@ def main():
 
         r_mean = np.mean(r_val_ls)
         r_std = np.std(r_val_ls)
-        print(model_name, skipped)
+        if skipped > 0:
+            print(model_name, skipped)
         print(model_name, r_mean, r_std)
 
         r_val_df[model_name] = [r_mean, r_std]
 
-    # Export age_predictions_all and r_val_df as csv
-    # age_predictions_all.to_csv(experiment_dir / 'age_predictions_allmodels.csv')
+    # Export r_val_df as csv
     r_val_df.to_csv(experiment_dir / 'r_values_allmodels.csv')
 
-    # ----------------------------------------
-    # Calculate age-BrainAGE correlation
-    # #TODO: delete this part in final version
-    # TODO: get standard deviation for this
-    # Note: this has now been corrected in previous scripts but keeping it here
-    # to assess existing models
-    for model_name in model_ls:
-        age_error_corr, _ = spearmanr(
-            (age_predictions_all[model_name] - age_predictions_all['Age']),
-            age_predictions_all['Age'])
-        print(model_name, age_error_corr)
+    # ---------------------------------------------------
+    # Get age-BrainAGE correlations for all models
 
+    # Create df to hold age-brainAGE correlation values for all models
+    age_brainage_corr_df = pd.DataFrame()
+
+    for model_name in model_ls:
+        model_dir = experiment_dir / model_name
+        file_name = model_dir / 'age_predictions.csv'
+
+        try:
+            model_data = pd.read_csv(file_name)
+        except FileNotFoundError:
+            print(f'No age prediction file for {model_name}.')
+            raise
+
+        repetition_cols = model_data.loc[:,
+                        'Prediction repetition 00' : 'Prediction repetition 09']
+        repetition_col_names = repetition_cols.columns
+
+        # Loop over all models to obtain age-BrainAGE spearman's rho value for each
+        age_brainage_corr_ls = []
+
+        # Initialise variable 'skipped' that counts how often correlation
+        # value could not be calculated (possibly due to model non-convergence)
+        skipped = 0
+
+        for i_col in repetition_col_names:
+            age_error_corr, _ = spearmanr(
+                (model_data[i_col] - model_data['Age']),
+                model_data['Age'])
+            if not np.isnan(age_error_corr):
+                age_brainage_corr_ls.append(age_error_corr)
+            else:
+                skipped += 1
+
+        age_brainage_corr_mean = np.mean(age_brainage_corr_ls)
+        age_brainage_corr_std = np.std(age_brainage_corr_ls)
+        if skipped > 0:
+            print(model_name, skipped)
+        print(model_name, age_brainage_corr_mean, age_brainage_corr_std)
+
+        age_brainage_corr_df[model_name] = \
+            [age_brainage_corr_mean, age_brainage_corr_std]
+
+    # Export age_brainage_corr_df as csv
+    age_brainage_corr_df.to_csv(experiment_dir / 'age_brainage_corr_allmodels.csv')
 
 if __name__ == '__main__':
     main()
