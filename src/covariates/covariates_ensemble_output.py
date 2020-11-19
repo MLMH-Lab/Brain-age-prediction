a5702410a839b34efccb713374de11e8ae0000ba
leabaecker <k1802521@kcl.ac.uk>
2020-11-19 12:05:52 +0000

    Updated script to also work for site2 data

--- src/covariates/covariates_ensemble_output.py
+++ src/covariates/covariates_ensemble_output.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""Create variables for average age predictions and prediction errors,
-incl. Brain Age Gap Estimate (BrainAGE), Brain Age Gap Estimate Residualised
-(BrainAGER) [1]
+"""Create variables for average age predictions and prediction errors for
+one model at a time, incl. Brain Age Gap Estimate (BrainAGE),
+Brain Age Gap Estimate Residualised (BrainAGER) [1]
 
 References:
 [1] Le TT, Kuplicki RT, McKinney BA, Yeh H-W, Thompson WK, Paulus MP and Tulsa
@@ -32,7 +32,7 @@ args = parser.parse_args()
 
 def get_brainager(age, predicted_age):
     """Calculates BrainAGER, the residual error of the regression of
-    chronological and predicted brain age
+    chronological and predicted brain age [1]
 
     Parameters
     ----------
@@ -62,33 +62,41 @@ def main(experiment_name, model_name):
     correlation_dir = experiment_dir / 'correlation_analysis'
     correlation_dir.mkdir(exist_ok=True)
 
-    # Load age predictions.
-    # Note: this assumes that the comparison analysis for the model of interest
-    # has been run
-    age_predictions_df = pd.read_csv(model_dir / 'age_predictions.csv')
+    # ------------------------------
+    # Load age predictions and get names of age prediction columns
+    # Note: this assumes that the comparison analysis for the model has been run
+    n_repetitions = 10
+    n_folds = 10
+    repetition_column_name = []
+
+    if experiment_name == 'biobank_scanner1':
+        age_predictions_df = pd.read_csv(model_dir / 'age_predictions.csv')
+        for i_repetition in range(n_repetitions):
+            repetition_column_name.append(
+                f'Prediction repetition {i_repetition:02d}')
+
+    elif experiment_name == 'biobank_scanner2':
+        age_predictions_df = pd.read_csv(model_dir / 'age_predictions_test.csv')
+        for i_repetition in range(n_repetitions):
+            for i_fold in range(n_folds):
+                repetition_column_name.append(
+                    f'Prediction {i_repetition:02d}_{i_fold:02d}')
 
     ensemble_df = age_predictions_df[['image_id', 'Age']]
 
-    n_repetitions = 10
-    repetition_column_name = []
-    for i_repetition in range(n_repetitions):
-        repetition_column_name.append(f'Prediction repetition {i_repetition:02d}')
+    # Add new column to ensemble_df for mean age predictions for each subject across all model repetitions
+    ensemble_df['Mean_predicted_age'] = \
+        age_predictions_df[repetition_column_name].mean(axis=1)
 
-    ensemble_df['Mean_predicted_age'] = age_predictions_df[repetition_column_name].mean(axis=1)
+    # Add new column to ensemble_df for age prediction error BrainAGE
+    ensemble_df['BrainAGE'] = \
+        ensemble_df['Mean_predicted_age'] - ensemble_df['Age']
 
-    # Add new columns to ensemble_df for age prediction error BrainAGE (Brain
-    # Age Gap Estimate)
-    # BrainAGE is the difference between mean predicted and chronological age
-    ensemble_df['BrainAGE_predmean'] = ensemble_df['Mean_predicted_age'] - ensemble_df['Age']
+    # Add new column to ensemble_df for BrainAGER
+    ensemble_df['BrainAGER'] = \
+        get_brainager(ensemble_df['Age'], ensemble_df['Mean_predicted_age'])
 
-    # Add new columns to ensemble_df for absolute BrainAGE
-    ensemble_df['Abs_BrainAGE_predmean'] = abs(ensemble_df['BrainAGE_predmean'])
-
-    # Add new columns to ensemble_df for BrainAGER (Brain Age Gap Estimate Residualised)
-    # BrainAGER is a more robust measure of age prediction error (see Le et al. 2018)
-    ensemble_df['BrainAGER_predmean'] = get_brainager(ensemble_df['Age'],
-                                                      ensemble_df['Mean_predicted_age'])
-
+    # Save ensemble output
     ensemble_df.to_csv(correlation_dir / f'ensemble_{model_name}_output.csv',
                        index=False)
 
