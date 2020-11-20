e53c2948d935a863c2d5de688aeb10625dfc6607
leabaecker <k1802521@kcl.ac.uk>
2020-11-20 12:35:37 +0000

    Corrected script to get variance explained from PCA models

--- src/preprocessing/compute_pca_variance_explained.py
+++ src/preprocessing/compute_pca_variance_explained.py
@@ -16,16 +16,21 @@ def main():
             pca_name = f'{i_repetition:02d}_{i_fold:02d}_pca.joblib'
             pca_name_ls.append(pca_name)
 
-    pca_dict = {}
+    pca_var_ls = []
     for i_model in pca_name_ls:
+        print(i_model)
         pca_model = load(pca_path / i_model)
-        var_explained = pca_model.explained_variance_ratio.sum()
-        pca_dict[pca_model] = var_explained
+        var_explained = pca_model.explained_variance_ratio_.sum()
+        pca_var_ls.append(var_explained)
 
-    pca_df = pd.DataFrame.from_dict(pca_dict)
+    pca_var_df = pd.DataFrame({'variance_explained':pca_var_ls})
+    var_mean = pca_var_df['variance_explained'].mean()
+    var_std = pca_var_df['variance_explained'].stdev()
+    print(var_mean, var_std)
+
     file_name = 'pca_variance_explained.csv'
-    file_path = PROJECT_ROOT / 'outputs' / 'pca' / file_name
-    pca_df.to_csv(file_path / file_name)
+    file_path = PROJECT_ROOT / 'outputs' / 'pca'
+    pca_var_df.to_csv(file_path / file_name)
 
 
 
